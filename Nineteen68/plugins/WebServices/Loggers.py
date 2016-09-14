af045c16800b7b253d72a20abb8a7fb479d3a706
Rakesh S V <rakesh.v@slkgroup.com>
2016-09-15 05:17:57 +0530

    Loggers.py for logging module

---
+++ Nineteen68/plugins/WebServices/Loggers.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sushma.p
+#
+# Created:     14-09-2016
+# Copyright:   (c) sushma.p 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+class Loggers:
+
+    def printLogger(self,msg):
+        #print timings
+        print msg
+
+
