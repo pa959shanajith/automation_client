04a8d841091e78e893f6ab48ae306bcc26765cf3
Prudhvi Prem Gujjuboyina <prudhvi.gujjuboyina@slkgroup.com>
2016-09-15 05:06:09 +0530

    Upload new file

---
+++ Nineteen68/plugins/Outlook/Logger.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        Logger
+# Purpose:
+#
+# Author:      prudhvi.gujjuboyina
+#
+# Created:     14-09-2016
+# Copyright:   (c) prudhvi.gujjuboyina 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def log(message):
+
+    print message
