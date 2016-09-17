0d6fa5e7ba8dc0c9e5695eb672357b3b413d0c1b
Prudhvi Prem Gujjuboyina <prudhvi.gujjuboyina@slkgroup.com>
2016-09-17 07:40:53 +0530

    Updated outlook.py

--- Nineteen68/plugins/Outlook/outlook.py
+++ Nineteen68/plugins/Outlook/outlook.py
@@ -11,8 +11,22 @@
 
 from  win32com.client import Dispatch
 import Logger
+import constants
 ## This class will have the methods to automate Outlook Application
 class OutlookKeywords:
+
+        def __init__(self, senderEmail,subject,toMail):
+            self.senderEmail=senderEmail
+            self.subject=subject
+            self.toMail=toMail
+            self.Flag=False
+            self.Subject=""
+            self.ToMailID=""
+            self.FromMailId=""
+            self.AttachmentStatus=constants.ATTACH_STATUS_NO
+            self.Body=""
+            self.message=""
+
 
 ##  This method takes 3 params
 ##  1. senderName (Display name  in Outlook application)
@@ -20,114 +34,91 @@ class OutlookKeywords:
 ##  3.to whom mail has been sent
 ##  On success return the mail content in string format
 ## else return fail
-        def GetEmail(self, senderEmail, subject, toMail):
-            global Flag
-            Flag=False
-            global Subject
-            Subject=""
-            global ToMailID
-            ToMailID=""
-            global FromMailId
-            FromMailId=""
-            global AttachmentStatus
-            AttachmentStatus="No"
-            global Body
-            Body=""
-            message=""
+        def GetEmail(self):
+
 ##   Get the Outlook com object registry
             outlook = Dispatch("Outlook.Application").GetNamespace("MAPI")
-            inbox = outlook.GetDefaultFolder("6")
+            inbox = outlook.GetDefaultFolder(constants.INBOX_FOLDER)
             all_inbox = inbox.Items
             folders = inbox.Folders
             for msg in all_inbox:
                 if msg.Class==43:
 ##                    if msg.SenderEmailType=='EX':
-##                            if msg.Sender.GetExchangeUser().PrimarySmtpAddress=="chethan.singh@slkgroup.com":
-##                            if msg.SenderEmailAddress=="prudhvi.gujjuboyina@slkgroup.com":
-
-                                if msg.SenderName==senderEmail:
-                                    if msg.Subject==subject:
-                                        if msg.To==toMail:
-##                                            fileName="D:/Python Mails/MyEmail.msg"
-##                                            os.makedirs(os.path.dirname(filename), exist_ok=True)
-##                                            with open(filename, "w") as f:
-##                                                f.write("FOOBAR")
-##                                            mail=   msg.SaveAs("D://abc.msg",3)
-##                                            print "-----------------"
-##                                            global Subject
-                                            Subject = msg.Subject
-                                            ToMailID= msg.To
-                                            Body= msg.Body
+                                if msg.SenderName==self.senderEmail:
+                                    if msg.Subject==self.subject:
+                                        if msg.To==self.toMail:
+                                            self.Subject = msg.Subject
+                                            self.Body= msg.Body
                                             if msg.SenderEmailType=='EX':
-                                                FromMailId= msg.Sender.GetExchangeUser().PrimarySmtpAddress
+                                                 self.FromMailId= msg.Sender.PropertyAccessor.GetProperty(constants.PR_SMTP_ADDRESS)
                                             else:
-                                                FromMailId=msg.SenderEmailAddress
+                                                self.FromMailId=msg.SenderEmailAddress
+                                            for recipient in  msg.Recipients:
+                                                self.ToMailID=recipient.PropertyAccessor.GetProperty(constants.PR_SMTP_ADDRESS)+";"
+                                                self.ToMailID=self.ToMailID[:-1]
                                             if msg.Attachments.Count>0:
-                                                AttachmentStatus="Yes"
-                                            else:
-                                                AttachmentStatus="No"
-##                                            print "From : "+msg.SenderName+" <"+msg.Sender.GetExchangeUser().PrimarySmtpAddress+">"+"\n" + "To: "+ msg.To + "\n"+ "Date: "+str(msg.SentOn) + "\n" + "Subject: "+ msg.Subject+ "\n" + str(msg.Attachments.Count)+ " attachments." + "\n"+ msg.Body
-
-##                                            print "-----------------"
-
-
-                                            message="From : "+msg.SenderName+" <"+FromMailId+">"+"\n"+ "To: "+ msg.To + "\n"+ "Date: "+str(msg.SentOn)+ "\n"+ "Subject: "+ msg.Subject+ "\n"+ str(msg.Attachments.Count)+ " attachments." + "\n"+ msg.Body
-
-##                                            print message
-
-                                            Flag=True
+                                                self.AttachmentStatus=constants.ATTACH_STATUS_YES
+                                            message="From : "+msg.SenderName+" <"+self.FromMailId+">"+"\n"+ "To: "+ self.ToMailID + "\n"+ "Date: "+str(msg.SentOn)+ "\n"+ "Subject: "+ msg.Subject+ "\n"+ str(msg.Attachments.Count)+ " attachments." + "\n"+ msg.Body
+                                            self.Flag=True
                                             msg.Display(False)
                                             return True
-                                        else :
-                                            Logger.log("Error : No such mail found")
-                                            return False
-
-
+                                        else:
+                                            continue
+                                    else:
+                                        continue
+                                else :
+                                    continue
+            if self.Flag!=True:
+                Logger.log("Error: No such mail found")
 
         def GetFromMailId(self):
-            if Flag==True:
-                print FromMailId
-                return FromMailId
+            if self.Flag==True:
+                return self.FromMailId
             else:
                 Logger.log("Error : No such mail id found")
-                return "fail"
+                return constants.STATUS_FAIL
 
 
         def GetAttachmentStatus(self):
-            if Flag==True:
-                print AttachmentStatus
-                return AttachmentStatus
+            if self.Flag==True:
+                return self.AttachmentStatus
             else:
                 Logger.log("Error : mail does't have such info")
-                return "fail"
+                return constants.STATUS_FAIL
 
         def GetSubject(self):
-            if Flag==True:
-                print Subject
-                return Subject
+            if self.Flag==True:
+                return self.Subject
             else:
                 Logger.log("Error : No subject found")
-                return "fail"
+                return constants.STATUS_FAIL
 
         def GetToMailID(self):
-            if Flag==True:
-                print ToMailID
-                return ToMailID
+            if self.Flag==True:
+                return self.ToMailID
             else:
                 Logger.log("Error : No such mail id found")
-                return "fail"
+                return constants.STATUS_FAIL
 
         def GetBody(self):
-            if Flag==True:
-                print Body
+            if self.Flag==True:
+                return self.Body
+            else:
+                Logger.log("Error : No Body found")
+                return constants.STATUS_FAIL
+
+        def VerifyEmail(self):
+            if self.Flag==True:
                 return Body
             else:
                 Logger.log("Error : No Body found")
-                return "fail"
+                return constants.STATUS_FAIL
 
-##a=OutlookKeywords()
+##a=OutlookKeywords("Microsoft Outlook","Your mailbox is almost full.","Prudhvi Prem Gujjuboyina")
 ##a.GetEmail()
-##a.GetFromMailId()
+##b=OutlookKeywords("Arpitha B.V","RE: Want to work on weekends !!!!!!!!!!!!!!!","Vivek Kallaje; Test Automation")
+##b.GetEmail()
+##b.GetEmail()
 ##a.GetToMailID()
 ##a.GetSubject()
 ##a.GetBody()
