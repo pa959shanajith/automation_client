0413ed76d74a6e49dabfa8e99fa5344f4b496059
Prudhvi Prem Gujjuboyina <prudhvi.gujjuboyina@slkgroup.com>
2016-09-17 07:42:08 +0530

    Adding Constants Module

---
+++ Nineteen68/plugins/Outlook/constants.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        Constants
+# Purpose:
+#
+# Author:      prudhvi.gujjuboyina
+#
+# Created:     17-09-2016
+# Copyright:   (c) prudhvi.gujjuboyina 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+PR_SMTP_ADDRESS ="http://schemas.microsoft.com/mapi/proptag/0x39FE001E"
+ATTACH_STATUS_NO="No"
+ATTACH_STATUS_YES="Yes"
+STATUS_FAIL="fail"
+INBOX_FOLDER="6"
