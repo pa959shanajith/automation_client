f7ae6ce82979c1bbcb4b201e6fbb13aabc8f80f8
Sushma G P <sushma.p@slkgroup.com>
2016-09-17 05:55:54 +0530

    Webservice constants file

---
+++ Nineteen68/plugins/WebServices/WSConstants.py
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sushma.p
+#
+# Created:     16-09-2016
+# Copyright:   (c) sushma.p 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+METHOD_GET='GET'
+
+METHOD_POST='POST'
+
+METHOD_PUT='PUT'
+
+METHOD_DELETE='DELETE'
+
+METHOD_HEAD='HEAD'
+
+METHOD_INVALID_INPUT='Invalid Input'
+
+METHOD_INVALID='Invalid Method'
+
+METHOD_ARRAY=[METHOD_GET,METHOD_POST,METHOD_PUT,METHOD_HEAD,METHOD_DELETE]
+
+RESPONSE_HEADER='Response header is:'
+
+RESPONSE_BODY='Response body is:'
+
+RESULT='Result is:'
+
+CONTENT_TYPE_XML = """{'content-type': 'text/xml'}"""
+
+CONTENT_TYPE_JSON = """{'content-type': 'application/json'}"""
+
