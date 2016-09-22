8715bd8bcf6b3d35950be21f272bb09bf21df2ea
Prudhvi Prem Gujjuboyina <prudhvi.gujjuboyina@slkgroup.com>
2016-09-22 05:41:12 +0530

    added new constants

--- Nineteen68/plugins/Outlook/constants.py
+++ Nineteen68/plugins/Outlook/constants.py
@@ -10,8 +10,14 @@
 #-------------------------------------------------------------------------------
 
 
-PR_SMTP_ADDRESS ="http://schemas.microsoft.com/mapi/proptag/0x39FE001E"
-ATTACH_STATUS_NO="No"
-ATTACH_STATUS_YES="Yes"
-STATUS_FAIL="fail"
-INBOX_FOLDER="6"
+PR_SMTP_ADDRESS ='http://schemas.microsoft.com/mapi/proptag/0x39FE001E'
+
+ATTACH_STATUS_NO='No'
+
+ATTACH_STATUS_YES='Yes'
+
+STATUS_FAIL='fail'
+
+INBOX_FOLDER='6'
+
+METHOD_INVALID='Invalid Method'
