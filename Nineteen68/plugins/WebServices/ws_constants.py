2c8ad835f95d787c0a49ead0a2d012437ee4f4e2
Prudhvi Prem Gujjuboyina <prudhvi.gujjuboyina@SLKSOFT.com>
2016-09-23 18:34:31 +0530

    Integrated with master

---
+++ Nineteen68/plugins/WebServices/ws_constants.py
@@ -0,0 +1,47 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sushma.p
+#
+# Created:     16-09-2016
+# Copyright:   (c) sushma.p 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+METHOD_GET='GET'
+
+METHOD_POST='POST'
+
+METHOD_PUT='PUT'
+
+METHOD_DELETE='DELETE'
+
+METHOD_HEAD='HEAD'
+
+METHOD_INVALID_INPUT='Invalid Input'
+
+METHOD_INVALID='Invalid Method'
+
+METHOD_ARRAY=[METHOD_GET,METHOD_POST,METHOD_PUT,METHOD_HEAD,METHOD_DELETE]
+
+RESPONSE_HEADER='Response header is:'
+
+RESPONSE_BODY='Response body is:'
+
+RESULT='Result is:'
+
+CONTENT_TYPE_XML = 'text/xml';
+
+CONTENT_TYPE_JSON = 'application/json'
+
+CONTENT_TYPE_SOAP_XML = 'application/soap+xml'
+
+##TYPE_XML="""Content-Type:text/xml;"""
+##
+##TYPE_JSON="""Content-Type:application/json;"""
+##
+##TYPE_SOAP_XML="""Content-Type:application/soap+xml;"""
+
+
+
