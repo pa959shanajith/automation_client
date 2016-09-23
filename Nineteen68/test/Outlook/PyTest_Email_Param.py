945ffb52df0c225f94251a9d66f1467e97d72723
Prudhvi Prem Gujjuboyina <prudhvi.gujjuboyina@slkgroup.com>
2016-09-24 04:08:53 +0530

    Updated PyTest_Email_Param.py

--- Nineteen68/test/Outlook/PyTest_Email_Param.py
+++ Nineteen68/test/Outlook/PyTest_Email_Param.py
@@ -14,8 +14,8 @@ import pytest
 import os
 import sys
 import xlrd
-##curdir  = os.curdir()
 os.chdir('..')
+
 maindir = os.getcwd()
 plug_path = maindir + '\Nineteen68\plugins\Outlook'
 sys.path.append(plug_path)
@@ -27,22 +27,17 @@ import dispatcher
 ##class outlooktestcase():
 
 ##a=outlook.OutlookKeywords()
-test_path = maindir + '\Nineteen68\\test'
+test_path = maindir + '\Nineteen68\\test\Outlook'
+os.chdir(test_path)
 test_data=testdata.read_excel_data(test_path+"\Email Keywords.xlsx")
-##message=testdata.message
-##for message in test_data:
-##    print message
-
 
 @pytest.fixture(params=test_data)
-##@pytest.mark.parametrize("frommail,subject,toMail",test_data )
 def message(request):
         return request.param
 
 
-    ## Verify if the email has been fetched successfully or not. Parameters are: "From" email ID, "Subject" and "To" email ID.
+
 def test_outlook_GetEmail(message):
-##    a=outlook.OutlookKeywords(message.fromMail, message.subject,  message.toMail)
     a=dispatcher.Dispatcher()
     assert a.dispatcher('GetEmail',message.fromMail, message.toMail, message.subject )
     assert a.dispatcher('GetFromMailId')==message.fromMail_exp
@@ -56,8 +51,4 @@ def test_outlook_GetEmail(message):
 
 
 
-
-   ##
-##        print "Printing the data of row:", row
-##      print fromMail,fromMail_exp,subject,subject_exp,toMail,toMail_exp
 
