6220c35692ff74a2d5c87c18a38a47d5d5532dca
Rakesh S V <rakesh.v@slkgroup.com>
2016-10-14 05:14:52 +0530

    changes in constant string

--- Nineteen68/plugins/Generic/generic_constants.py
+++ Nineteen68/plugins/Generic/generic_constants.py
@@ -18,13 +18,13 @@ INPUT_IS='Input is:'
 
 FILE_NOT_EXISTS='File does not exists'
 
-FILE_TYPES=['.txt','.xls','.xlsx','.pdf','.xml']
+FILE_TYPES=['.txt','xls','.xlsx','.pdf']
 
 INVALID_FILE_FORMAT='Invalid file format'
 
 INDEX_EXCEEDS='Index of row/col is exceeded'
 
-INP_VAL_EXCEEDS = 'Given length should be in (1-323) range'
+INP_VAL_EXCEEDS = 'Given length should be greater than 0'
 
 DATE_FORMAT = '%d/%m/%Y'
 
