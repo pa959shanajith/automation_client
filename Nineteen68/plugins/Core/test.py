728eafdd2475c8b4d1b1ff09a0fd2b01751c18e1
Sushma G P <sushma.p@SLKSOFT.com>
2016-11-03 12:09:43 +0530

    Added documentation and fixed few issues in Handler

--- Nineteen68/plugins/Core/test.py
+++ Nineteen68/plugins/Core/test.py
@@ -87,8 +87,201 @@ data=[{ 	"template": "", 	"testscript_name": "getParam", 	"testcase":
     "url": "",
     "custname": "@Generic"
   },
+
+  {
+    "outputVal": "",
+    "keywordVal": "createDynVariable",
+    "objectName": " ",
+    "inputVal": [
+      "{10};qwert"
+    ],
+    "appType": "Generic",
+    "stepNo": 8,
+    "url": " ",
+    "custname": "@Generic"
+  },
+  {
+    "outputVal": "",
+    "keywordVal": "displayVariableValue",
+    "objectName": " ",
+    "inputVal": [
+      "{10}"
+    ],
+    "appType": "Generic",
+    "stepNo": 9,
+    "url": " ",
+    "custname": "@Generic"
+  },
+
+  {
+    "outputVal": "",
+    "keywordVal": "displayVariableValue",
+    "objectName": "",
+    "inputVal": [
+      "{20}"
+    ],
+    "appType": "Generic",
+    "stepNo": 11,
+    "url": "",
+    "custname": "@Generic"
+  },
+
   {
     "outputVal": "##",
+    "keywordVal": "endIf",
+    "objectName": "",
+    "inputVal": [
+      ""
+    ],
+    "appType": "Generic",
+    "stepNo": 13,
+    "url": "",
+    "custname": "@Generic"
+  },
+  {
+    "comments": ""
+  }
+]
+}]
+
+data=[{ 	"template": "", 	"testscript_name": "getParam", 	"testcase":
+   [
+  {
+    "outputVal": "",
+    "keywordVal": "for",
+    "objectName": " ",
+    "inputVal": [
+      "(10;==;20)"
+    ],
+    "appType": "Generic",
+    "stepNo": 1,
+    "url": " ",
+    "custname": "@Generic"
+  },
+  {
+    "outputVal": "",
+    "keywordVal": "pause",
+    "objectName": "",
+    "inputVal": [
+      ""
+    ],
+    "appType": "Generic",
+    "stepNo": 2,
+    "url": "",
+    "custname": "@Generic"
+  },
+  {
+    "outputVal": "",
+    "keywordVal": "for",
+    "objectName": "",
+    "inputVal": [
+      ""
+    ],
+    "appType": "Generic",
+    "stepNo": 2,
+    "url": "",
+    "custname": "@Generic"
+  },
+  {
+    "outputVal": "",
+    "keywordVal": "pause",
+    "objectName": "",
+    "inputVal": [
+      ""
+    ],
+    "appType": "Generic",
+    "stepNo": 2,
+    "url": "",
+    "custname": "@Generic"
+  },
+
+  {
+    "outputVal": "",
+    "keywordVal": "endFor",
+    "objectName": "",
+    "inputVal": [
+      ""
+    ],
+    "appType": "Generic",
+    "stepNo": 13,
+    "url": "",
+    "custname": "@Generic"
+  },
+  {
+    "outputVal": "",
+    "keywordVal": "endfor",
+    "objectName": "",
+    "inputVal": [
+      ""
+    ],
+    "appType": "Generic",
+    "stepNo": 13,
+    "url": "",
+    "custname": "@Generic"
+  },
+  {
+    "comments": ""
+  }
+]
+}]
+
+dataList=[]
+dataList.append(data)
+##data=[{"template":"","testscript_name":"getParam","testcase":[   {     "outputVal": "",     "keywordVal": "for",     "objectName": "",     "inputVal": [       "3"     ],     "appType": "Generic",     "stepNo": 1,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "createDynVariable",     "objectName": " ",     "inputVal": [       "{jp2}; inside for"     ],     "appType": "Generic",     "stepNo": 2,     "url": " ",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "for",     "objectName": "",     "inputVal": [       "2"     ],     "appType": "Generic",     "stepNo": 3,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "displayVariableValue",     "objectName": "",     "inputVal": [       "{jp2}"     ],     "appType": "Generic",     "stepNo": 4,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "endFor",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 5,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "endFor",     "objectName": " ",     "inputVal": [       "DATE_OPERATIONS"     ],     "appType": "Generic",     "stepNo": 6,     "url": " ",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "endFor",     "objectName": " ",     "inputVal": [       "DATE_OPERATIONS"     ],     "appType": "Generic",     "stepNo": 7,     "url": " ",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "if",     "objectName": "",     "inputVal": [       "(sjhfsj)"     ],     "appType": "Generic",     "stepNo": 8,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "copyValue",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 9,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "endIf",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 10,     "url": "",     "custname": "@Generic"   },   {     "comments": ""   } ]}]
+##data=[{"template":"","testscript_name":"Script1","testcase":[   {     "outputVal": "",     "keywordVal": "copyValue",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 1,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "getParam",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 2,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "startLoop",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 3,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "pause",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 4,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "endLoop",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 5,     "url": "",     "custname": "@Generic"   },   {     "comments": ""   } ]}]
+data=[{ 	"template": "", 	"testscript_name": "getParam", 	"testcase":
+   [
+  {
+    "outputVal": "",
+    "keywordVal": "if",
+    "objectName": " ",
+    "inputVal": [
+      "(10;==;20)"
+    ],
+    "appType": "Generic",
+    "stepNo": 1,
+    "url": " ",
+    "custname": "@Generic"
+  },
+
+  {
+    "outputVal": "{con1}",
+    "keywordVal": "concatenate",
+    "objectName": "",
+    "inputVal": [
+      "12;23"
+    ],
+    "appType": "Generic",
+    "stepNo": 4,
+    "url": "",
+    "custname": "@Generic"
+  },
+  {
+    "outputVal": "",
+    "keywordVal": "displayVariableValue",
+    "objectName": "",
+    "inputVal": [
+      "{con1}"
+    ],
+    "appType": "Generic",
+    "stepNo": 5,
+    "url": "",
+    "custname": "@Generic"
+  },
+  {
+    "outputVal": "",
+    "keywordVal": "else",
+    "objectName": "",
+    "inputVal": [
+      ""
+    ],
+    "appType": "Generic",
+    "stepNo": 6,
+    "url": "",
+    "custname": "@Generic"
+  },
+  {
+    "outputVal": "",
     "keywordVal": "if",
     "objectName": " ",
     "inputVal": [
@@ -160,7 +353,7 @@ data=[{ 	"template": "", 	"testscript_name": "getParam", 	"testcase":
     "custname": "@Generic"
   },
   {
-    "outputVal": "##",
+    "outputVal": "",
     "keywordVal": "endIf",
     "objectName": "",
     "inputVal": [
@@ -176,11 +369,14 @@ data=[{ 	"template": "", 	"testscript_name": "getParam", 	"testcase":
   }
 ]
 }]
-
-dataList=[]
-dataList.append(data)
-##data=[{"template":"","testscript_name":"getParam","testcase":[   {     "outputVal": "",     "keywordVal": "for",     "objectName": "",     "inputVal": [       "3"     ],     "appType": "Generic",     "stepNo": 1,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "createDynVariable",     "objectName": " ",     "inputVal": [       "{jp2}; inside for"     ],     "appType": "Generic",     "stepNo": 2,     "url": " ",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "for",     "objectName": "",     "inputVal": [       "2"     ],     "appType": "Generic",     "stepNo": 3,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "displayVariableValue",     "objectName": "",     "inputVal": [       "{jp2}"     ],     "appType": "Generic",     "stepNo": 4,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "endFor",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 5,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "endFor",     "objectName": " ",     "inputVal": [       "DATE_OPERATIONS"     ],     "appType": "Generic",     "stepNo": 6,     "url": " ",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "endFor",     "objectName": " ",     "inputVal": [       "DATE_OPERATIONS"     ],     "appType": "Generic",     "stepNo": 7,     "url": " ",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "if",     "objectName": "",     "inputVal": [       "(sjhfsj)"     ],     "appType": "Generic",     "stepNo": 8,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "copyValue",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 9,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "endIf",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 10,     "url": "",     "custname": "@Generic"   },   {     "comments": ""   } ]}]
-data=[{"template":"","testscript_name":"Script1","testcase":[   {     "outputVal": "",     "keywordVal": "copyValue",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 1,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "getParam",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 2,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "startLoop",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 3,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "pause",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 4,     "url": "",     "custname": "@Generic"   },   {     "outputVal": "",     "keywordVal": "endLoop",     "objectName": "",     "inputVal": [       ""     ],     "appType": "Generic",     "stepNo": 5,     "url": "",     "custname": "@Generic"   },   {     "comments": ""   } ]}]
 dataList.append(data)
 for d in dataList:
-    obj.parse_json(d)
+    flag=obj.parse_json(d)
+    if flag == False:
+        break
+print '\n'
+obj.read_step()
+
+
+
+
--- Nineteen68/plugins/Core/teststepproperty.py
+++ Nineteen68/plugins/Core/teststepproperty.py
@@ -8,11 +8,12 @@
 # Copyright:   (c) sushma.p 2016
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-
+import logger
 class TestStepProperty():
 
-    def __init__(self,keyword,apptype,inputval,objectname,outputval,stepnum,url,custname,testscriptname):
+    def __init__(self,keyword,index,apptype,inputval,objectname,outputval,stepnum,url,custname,testscript_name):
         self.keyword=keyword
+        self.index=index
         self.apptype=apptype
         self.inputval=inputval
         self.objectname=objectname
@@ -20,12 +21,10 @@ class TestStepProperty():
         self.stepnum=stepnum
         self.url=url
         self.custname=custname
-        self.testscriptname=testscriptname
+        self.testscript_name=testscript_name
 
     def print_step(self):
-##        print step.keyword,step.apptype,step.inputval,step.objectname,step.outputval,step.stepnum,step.url,step.custname,step.testscriptname+'\n'
-        print self.stepnum,self.keyword,self.apptype,self.inputval,self.objectname,self.outputval,self.url,self.custname,self.testscriptname+'\n'
-
+        logger.log(str(self.index)+' '+self.keyword+' '+self.testscript_name+' '+self.apptype)
 
 
 
