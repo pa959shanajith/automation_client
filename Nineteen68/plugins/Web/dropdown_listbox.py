21aa7e3061887ba7b657ccfa47e92b3c040d3810
Rakesh S V <rakesh.v@slkgroup.com>
2016-12-01 08:56:03 +0530

    defect fixes on input value validation

--- Nineteen68/plugins/Web/dropdown_listbox.py
+++ Nineteen68/plugins/Web/dropdown_listbox.py
@@ -47,6 +47,8 @@ class DropdownKeywords():
                                     select.select_by_index(input_val)
                                     status=webconstants.TEST_RESULT_PASS
                                     result=webconstants.TEST_RESULT_TRUE
+                    else:
+                        logger.log('Invalid input')
 
                 except Exception as e:
                     Exceptions.error(e)
@@ -109,6 +111,8 @@ class DropdownKeywords():
                         select.select_by_visible_text(inp_val)
                         status=webconstants.TEST_RESULT_PASS
                         result=webconstants.TEST_RESULT_TRUE
+                    else:
+                        logger.log('Invalid input')
 
                 except Exception as e:
                     Exceptions.error(e)
@@ -140,6 +144,9 @@ class DropdownKeywords():
                         if (first_value == inp_val):
                             status=webconstants.TEST_RESULT_PASS
                             result=webconstants.TEST_RESULT_TRUE
+
+                    else:
+                        logger.log('Invalid input')
 
                 except Exception as e:
                     Exceptions.error(e)
@@ -174,6 +181,9 @@ class DropdownKeywords():
                             status=webconstants.TEST_RESULT_PASS
                             result=webconstants.TEST_RESULT_TRUE
 
+                    else:
+                        logger.log('Invalid input')
+
                 except Exception as e:
                     Exceptions.error(e)
         return status,result
@@ -189,43 +199,45 @@ class DropdownKeywords():
         status=webconstants.TEST_RESULT_FAIL
         result=webconstants.TEST_RESULT_FALSE
         visibilityFlag=True
-        if webelement is not None:
+        try:
+            if input is not None:
+                if webelement is not None:
 
-            try:
-##                    utilobj=UtilWebKeywords()
-##                    isvisble=utilobj.is_visible(webelement)
-##                    if len(args)>0:
-##                        visibilityFlag=args[0]
-##                        logger.log('Input is '+input)
-##                    if input is not None:
-##                        if not(visibilityFlag and isvisble):
-                select = Select(webelement)
-                option_len = select.options
-                opt_len = len(option_len)
-                inp_val_len = len(input)
-                temp = []
-                flag = True
-                for x in range(0,opt_len):
-                    internal_val = select.options[x].text
-                    temp.append(internal_val)
 
-                for y in range(0,inp_val_len):
-                    input_val = input[y]
-                    if (input_val in temp):
-                        temp.remove(input_val)
-                    else:
-                        flag = False
-                        break
+        ##                    utilobj=UtilWebKeywords()
+        ##                    isvisble=utilobj.is_visible(webelement)
+        ##                    if len(args)>0:
+        ##                        visibilityFlag=args[0]
+        ##                        logger.log('Input is '+input)
+        ##                    if input is not None:
+        ##                        if not(visibilityFlag and isvisble):
+                    select = Select(webelement)
+                    option_len = select.options
+                    opt_len = len(option_len)
+                    inp_val_len = len(input)
+                    temp = []
+                    flag = True
+                    for x in range(0,opt_len):
+                        internal_val = select.options[x].text
+                        temp.append(internal_val)
 
-                if(flag == True):
-                    status=webconstants.TEST_RESULT_PASS
-                    result=webconstants.TEST_RESULT_TRUE
+                    for y in range(0,inp_val_len):
+                        input_val = input[y]
+                        if (input_val in temp):
+                            temp.remove(input_val)
+                        else:
+                            flag = False
+                            break
 
-                else:
-                    logger.log('Provided input not present in element')
+                    if(flag == True):
+                        status=webconstants.TEST_RESULT_PASS
+                        result=webconstants.TEST_RESULT_TRUE
 
-            except Exception as e:
-                Exceptions.error(e)
+            else:
+                logger.log('Provided input not present in element')
+
+        except Exception as e:
+            Exceptions.error(e)
         return status,result
 
     def selectMultipleValuesByIndexes(self,webelement,input,*args):
@@ -238,29 +250,33 @@ class DropdownKeywords():
         status=webconstants.TEST_RESULT_FAIL
         result=webconstants.TEST_RESULT_FALSE
         visibilityFlag=True
-        if webelement is not None:
-            if webelement.is_enabled():
-                try:
-##                    utilobj=UtilWebKeywords()
-##                    isvisble=utilobj.is_visible(webelement)
-##                    if len(args)>0:
-##                        visibilityFlag=args[0]
-##                        logger.log('Input is '+input)
-##                    if input is not None:
-##                        if not(visibilityFlag and isvisble):
-                    count= len(input)
-                    select = Select(webelement)
-                    iList = select.options
-                    iListSize = len(iList)
-                    for x in range(0,count):
-                        input_val_temp = input[x]
-                        input_val = int(input_val_temp)
-                        select.select_by_index(input_val)
-                    status=webconstants.TEST_RESULT_PASS
-                    result=webconstants.TEST_RESULT_TRUE
+        try:
+            if input is not None:
+                if webelement is not None:
+                    if webelement.is_enabled():
 
-                except Exception as e:
-                    Exceptions.error(e)
+        ##                    utilobj=UtilWebKeywords()
+        ##                    isvisble=utilobj.is_visible(webelement)
+        ##                    if len(args)>0:
+        ##                        visibilityFlag=args[0]
+        ##                        logger.log('Input is '+input)
+        ##                    if input is not None:
+        ##                        if not(visibilityFlag and isvisble):
+                            count= len(input)
+                            select = Select(webelement)
+                            iList = select.options
+                            iListSize = len(iList)
+                            for x in range(0,count):
+                                input_val_temp = input[x]
+                                input_val = int(input_val_temp)
+                                select.select_by_index(input_val)
+                            status=webconstants.TEST_RESULT_PASS
+                            result=webconstants.TEST_RESULT_TRUE
+            else:
+                logger.log('Invalid input')
+
+        except Exception as e:
+            Exceptions.error(e)
         return status,result
 
     def getSelected(self,webelement,*args):
@@ -273,20 +289,22 @@ class DropdownKeywords():
         status=webconstants.TEST_RESULT_FAIL
         result=webconstants.TEST_RESULT_FALSE
         visibilityFlag=True
-        if webelement is not None:
-            if webelement.is_enabled():
-                try:
-                    utilobj=UtilWebKeywords()
-                    isvisble=utilobj.is_visible(webelement)
-                    check = webelement.get_attribute('multiple')
-                    if len(args)>0:
-                        visibilityFlag=args[0]
-##                        logger.log('Input is '+input)
-                    if input is not None:
-##                        if not(visibilityFlag and isvisble):
-                        select = Select(webelement)
-                        index = select.all_selected_options
-                        for i in range(0, len(index)):
+        try:
+            if input is not None:
+                if webelement is not None:
+                    if webelement.is_enabled():
+
+                        utilobj=UtilWebKeywords()
+                        isvisble=utilobj.is_visible(webelement)
+                        check = webelement.get_attribute('multiple')
+                        if len(args)>0:
+                            visibilityFlag=args[0]
+    ##                        logger.log('Input is '+input)
+                        if input is not None:
+    ##                        if not(visibilityFlag and isvisble):
+                            select = Select(webelement)
+                            index = select.all_selected_options
+    ##                        for i in range(0, len(index)):
                             if (len(index) == '1'):
                                 output = select.all_selected_options[i].text
                                 str(output)
@@ -296,16 +314,18 @@ class DropdownKeywords():
                             else:
                                 temp = []
                                 for x in range(0,len(index)):
-                                    out = select.all_selected_options[i].text
+                                    out = select.all_selected_options[x].text
                                     value = str(out)
                                     temp.append(value)
                                 output = ';'.join(temp)
                                 logger.log(output)
                                 status=webconstants.TEST_RESULT_PASS
                                 result=webconstants.TEST_RESULT_TRUE
+            else:
+                logger.log('Invalid input')
 
-                except Exception as e:
-                    Exceptions.error(e)
+        except Exception as e:
+            Exceptions.error(e)
         return status,result,output
 
     def selectMultipleValuesByText(self,webelement,input,*args):
@@ -341,35 +361,38 @@ class DropdownKeywords():
         status=webconstants.TEST_RESULT_FAIL
         result=webconstants.TEST_RESULT_FALSE
         visibilityFlag=True
-        if webelement is not None:
-            if webelement.is_enabled():
-                try:
-##                    utilobj=UtilWebKeywords()
-##                    isvisble=utilobj.is_visible(webelement)
-##                    if len(args)>0:
-##                        visibilityFlag=args[0]
-##                        logger.log('Input is '+input)
-##                    if input is not None:
-##                        if not(visibilityFlag and isvisble):
-                    count = len(input)
-                    select = Select(webelement)
-                    iList = select.options
-                    iListSize = len(iList)
-                    temp = []
-                    for x in range(0,count):
-                        input_val_temp = input[x]
-                        input_val = int(input_val_temp)
-                        out=select.options[input_val].text
-                        value = str(out)
-                        temp.append(value)
+        try:
+            if input is not None:
+                if webelement is not None:
+                    if webelement.is_enabled():
 
-                    output = ';'.join(temp)
-                    logger.log(output)
-                    status=webconstants.TEST_RESULT_PASS
-                    result=webconstants.TEST_RESULT_TRUE
+        ##                    utilobj=UtilWebKeywords()
+        ##                    isvisble=utilobj.is_visible(webelement)
+        ##                    if len(args)>0:
+        ##                        visibilityFlag=args[0]
+        ##                        logger.log('Input is '+input)
+        ##                    if input is not None:
+        ##                        if not(visibilityFlag and isvisble):
+                        count = len(input)
+                        select = Select(webelement)
+                        iList = select.options
+                        iListSize = len(iList)
+                        temp = []
+                        for x in range(0,count):
+                            input_val_temp = input[x]
+                            input_val = int(input_val_temp)
+                            out=select.options[input_val].text
+                            value = str(out)
+                            temp.append(value)
 
-                except Exception as e:
-                    Exceptions.error(e)
+                        output = ';'.join(temp)
+                        logger.log(output)
+                        status=webconstants.TEST_RESULT_PASS
+                        result=webconstants.TEST_RESULT_TRUE
+            logger.log('Invalid input')
+
+        except Exception as e:
+            Exceptions.error(e)
         return status,result,output
 
     def selectAllValues(self,webelement,*args):
@@ -396,9 +419,9 @@ class DropdownKeywords():
                                 select.select_by_visible_text(to_select)
                         wlist = select.all_selected_options
                         size = len(wlist)
-                        if(wlist == iListSize):
-                                status=webconstants.TEST_RESULT_PASS
-                                result=webconstants.TEST_RESULT_TRUE
+                        if(size == iListSize):
+                            status=webconstants.TEST_RESULT_PASS
+                            result=webconstants.TEST_RESULT_TRUE
 
                 except Exception as e:
                     Exceptions.error(e)
@@ -428,6 +451,8 @@ class DropdownKeywords():
                                     output=select.options[input_val].text
                                     status=webconstants.TEST_RESULT_PASS
                                     result=webconstants.TEST_RESULT_TRUE
+                    else:
+                        logger.log('Invalid input')
 
                 except Exception as e:
                     Exceptions.error(e)
@@ -437,39 +462,43 @@ class DropdownKeywords():
         status=webconstants.TEST_RESULT_FAIL
         result=webconstants.TEST_RESULT_FALSE
         visibilityFlag=True
-        if webelement is not None:
-            try:
-##                    utilobj=UtilWebKeywords()
-##                    isvisble=utilobj.is_visible(webelement)
-##                    if len(args)>0:
-##                        visibilityFlag=args[0]
-##                        logger.log('Input is '+input)
-##                    if input is not None:
-##                        if not(visibilityFlag and isvisble):
-                select = Select(webelement)
-                option_len = select.options
-                opt_len = len(option_len)
-                inp_val_len = len(input)
-                temp = []
-                flag = True
-                for x in range(0,opt_len):
-                    internal_val = select.options[x].text
-                    str(internal_val)
-                    temp.append(internal_val)
-                count = 0
-                for y in range(0,inp_val_len):
-                    input_temp = input[y]
-                    if (input_temp in temp):
-                        count+=1
+        try:
+            if input is not None:
+                if webelement is not None:
+
+        ##                    utilobj=UtilWebKeywords()
+        ##                    isvisble=utilobj.is_visible(webelement)
+        ##                    if len(args)>0:
+        ##                        visibilityFlag=args[0]
+        ##                        logger.log('Input is '+input)
+        ##                    if input is not None:
+        ##                        if not(visibilityFlag and isvisble):
+                    select = Select(webelement)
+                    option_len = select.options
+                    opt_len = len(option_len)
+                    inp_val_len = len(input)
+                    temp = []
+                    flag = True
+                    for x in range(0,opt_len):
+                        internal_val = select.options[x].text
+                        str(internal_val)
+                        temp.append(internal_val)
+                    count = 0
+                    for y in range(0,inp_val_len):
+                        input_temp = input[y]
+                        if (input_temp in temp):
+                            count+=1
+                        else:
+                            flag = False
+                    if(not(flag == False)):
+                        status=webconstants.TEST_RESULT_PASS
+                        result=webconstants.TEST_RESULT_TRUE
                     else:
-                        flag = False
-                if(not(flag == False)):
-                    status=webconstants.TEST_RESULT_PASS
-                    result=webconstants.TEST_RESULT_TRUE
-                else:
-                    logger.log('Inputs does not match')
-            except Exception as e:
-                Exceptions.error(e)
+                        logger.log('Inputs does not match')
+            else:
+                logger.log('Invalid input')
+        except Exception as e:
+            Exceptions.error(e)
         return status,result
 
     def deselectAll(self,webelement,*args):
@@ -489,7 +518,7 @@ class DropdownKeywords():
                         select = Select(webelement)
                         select.deselect_all()
                         check = select.all_selected_options
-                        if(len(check) > 0):
+                        if(len(check) == 0):
                             status=webconstants.TEST_RESULT_PASS
                             result=webconstants.TEST_RESULT_TRUE
 
