d5a016caaca9508fe9898632338c848a99f90bae
Prudhvi Prem Gujjuboyina <prudhvi.gujjuboyina@SLKSOFT.com>
2017-01-05 10:35:00 +0530

    adding web loggers for batch operation  keywords

--- Nineteen68/plugins/Generic/batch_operation_keyword.py
+++ Nineteen68/plugins/Generic/batch_operation_keyword.py
@@ -15,28 +15,43 @@ import os
 import logger
 import Exceptions
 from constants import *
+from loggermessages import *
+import logging
 
+
+log = logging.getLogger('batch_operation_keyword.py')
 class BatchOperationKeyword():
 
     def executeFile(self, filePath):
+        log.info(KEYWORD_EXECUTION_STARTED)
+        logger.print_on_console('Executing keyword : %s',executeFile)
         status=TEST_RESULT_FAIL
         methodoutput=TEST_RESULT_FALSE
+        error_msg=None
+        output_res=MD5_TEMP_RES
         try:
+            log.debug('reading the inputs')
             filename,file_ext=os.path.splitext(filePath)
             if file_ext in generic_constants.BATCH_FILE_TYPE or  file_ext in generic_constants.EXE_FILE_TYPE:
+                log.debug('executing .bat or .exe file')
                 logger.print_on_console('executing .bat or .exe file')
                 p = subprocess.Popen(filePath,cwd=os.path.dirname(filePath), creationflags=subprocess.CREATE_NEW_CONSOLE)
                 status=TEST_RESULT_PASS
                 methodoutput=TEST_RESULT_TRUE
             elif file_ext in generic_constants.VBS_FILE_TYPE:
+                log.debug('executing .vbs file')
                 logger.print_on_console('executing .vbs file')
                 os.chdir(os.path.dirname(filePath))
                 os.startfile(filePath)
                 status=TEST_RESULT_PASS
                 methodoutput=TEST_RESULT_TRUE
             else:
+                log.debug('Invalid file format')
                 logger.print_on_console(generic_constants.INVALID_FILE_FORMAT)
         except Exception as e:
-            Exceptions.error(e)
-        return status,methodoutput
+            log.error(e)
+            log.error(e.msg)
+            logger.print_on_console(e.msg)
+            error_msg=e.msg
+        return status,methodoutput,output_res,error_msg
 
