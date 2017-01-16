956e9f5a70db067eb36e30193e58705a7409cfb4
sushma.p <sushma.p@WSLK13FODC3-009.SLKSOFT.COM>
2017-01-16 17:20:41 +0530

    Added error codes for launch application in OEBS

--- Nineteen68/plugins/Oebs/oebs_msg.py
+++ Nineteen68/plugins/Oebs/oebs_msg.py
@@ -177,4 +177,27 @@ ELEMENT_FOUND=True
 
 OUTPUT_CONSTANT ="""9cc33d6fe25973868b30f4439f09901a"""
 
+APPLICATION_ERROR_CODES = {'0':'The operating system is out of memory or resources.',
+                          '2': 'The specified file was not found.',
+                          '3':'The specified path was not found.',
+                          '5':'The operating system denied access to the specified file',
+                          '8':'There was not enough memory to complete the operation.',
+                          '10':'Wrong Windows version',
+                          '11':'The .EXE file is invalid (non-Win32 .EXE or error in .EXE image)',
+                          '12':'Application was designed for a different operating system.',
+                          '13':'Application was designed for MS-DOS 4.0',
+                          '15':'Attempt to load a real-mode program.',
+                          '16':'Attempt to load a second instance of an application with non-readonly data segments',
+                          '19':'Attempt to load a compressed application file',
+                          '20':'Dynamic-link library (DLL) file failure.',
+                          '26':'A sharing violation occurred.',
+                          '27':'The filename association is incomplete or invalid.',
+                          '28':'The DDE transaction could not be completed because the request timed out..',
+                          '29':'The DDE transaction failed.',
+                          '30':'The DDE transaction could not be completed because other DDE transactions were being processed..',
+                          '31':'There is no application associated with the given filename extension.',
+                          '32':'The specified dynamic-link library was not found.'}
+
+
+
 
