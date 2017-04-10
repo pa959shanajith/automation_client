32fa6360749bd9af2637f865d0ee2f6a007b2106
Rakesh S V <rakesh.v@slkgroup.com>
2017-04-10 12:43:03 +0530

    Fixed app type miss match for mobility

--- Nineteen68/plugins/Core/constants.py
+++ Nineteen68/plugins/Core/constants.py
@@ -60,9 +60,9 @@ APPTYPE_SAP = 'sap'
 APPTYPE_DESKTOP_JAVA = 'desktopjava'
 
 
-APPTYPE_MOBILE='mobileweb'
+APPTYPE_MOBILE='MobileWeb'
 
-APPTYPE_MOBILE_APP = 'mobility'
+APPTYPE_MOBILE_APP = 'MobileApp'
 
 IGNORE_THIS_STEP = '<<Ignore.This.Step>>'
 
