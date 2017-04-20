dc93487b3652da50b67d12bc8274b96183c80dcb
anas.ahmed1 <anas.ahmed1@WSLK13FODC6-037.SLKSOFT.COM>
2017-04-20 20:26:35 +0530

    Made changes in dispatcher to the handle input of sendfunctionkeys

--- Nineteen68/plugins/SAP/sap_dispatcher.py
+++ Nineteen68/plugins/SAP/sap_dispatcher.py
@@ -118,7 +118,7 @@ class SAPDispatcher:
 
             keyword=keyword.lower()
             if keyword in dict.keys():
-                if keyword=='launch_application' or keyword=='LaunchApplication' or keyword=='launchapplication' or keyword=='starttransaction' :
+                if keyword=='launch_application' or keyword=='sendfunctionkeys' or keyword=='launchapplication' or keyword=='starttransaction' :
                     result= dict[keyword](input,output)
                 else:
                     result= dict[keyword](objectname,url,input,output)
