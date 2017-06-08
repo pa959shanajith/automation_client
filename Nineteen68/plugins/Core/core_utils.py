5f781359e3a4e80894daaaad4c8ff8cc96871623
vishvas.a <vishvas.a@WSLK13FODC6-047.SLKSOFT.COM>
2017-06-08 16:34:38 +0530

    Handled Exception

--- Nineteen68/plugins/Core/core_utils.py
+++ Nineteen68/plugins/Core/core_utils.py
@@ -17,17 +17,20 @@ class CoreUtils():
 
     #definition to fetch the data size in bytes/kilobytes/megabytes
     # sends data length in bytes if memoryformat not provided
-    def getdatasize(self,inputdata,memoryformat):
-        datalength = sys.getsizeof(inputdata)
-        log.info('Data size in bytes:')
-        log.info(datalength)
-        kilobytes = datalength/1024
-        if memoryformat == 'kb':
-            datalength = kilobytes
-            log.info('Data size is:',datalength)
-        elif memoryformat == 'mb':
-            datalength = kilobytes/1024
-            log.info('Data size is:',datalength)
-        else:
-            log.info('Data size is:',datalength)
-        return datalength
+    try:
+        def getdatasize(self,inputdata,memoryformat):
+            datalength = sys.getsizeof(inputdata)
+            log.info('Data size in bytes:')
+            log.info(datalength)
+            kilobytes = datalength/1024
+            if memoryformat == 'kb':
+                datalength = kilobytes
+                log.info('Data size is: %s',str(datalength))
+            elif memoryformat == 'mb':
+                datalength = kilobytes/1024
+                log.info('Data size is: %s',str(datalength))
+            else:
+                log.info('Data size is: %s',str(datalength))
+            return datalength
+    except Exception as e:
+        log.info(e)
