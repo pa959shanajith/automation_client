85fce3bbe1b5b90a4d6d5c1977d897c5f7501b94
anas.ahmed1 <anas.ahmed1@WSLK13FODC6-044.SLKSOFT.COM>
2017-07-06 19:34:19 +0530

    Added ClickCell and DoubleClickCell to sap_shell_keywords and sap_dispatcher

--- Nineteen68/plugins/SAP/sap_dispatcher.py
+++ Nineteen68/plugins/SAP/sap_dispatcher.py
@@ -167,7 +167,9 @@ class SAPDispatcher:
                   'selectrows':self.shell_keywords_obj.selectRows,
                   'toolbaractionkeys':self.shell_keywords_obj.toolBarActionKeys,
                   'presstoolbarbutton':self.shell_keywords_obj.pressToolBarButton,
-                  'getcelltext': self.shell_keywords_obj.getCellText
+                  'getcelltext': self.shell_keywords_obj.getCellText,
+                  'clickcell':self.shell_keywords_obj.clickCell,
+                  'doubleclickcell':self.shell_keywords_obj.doubleClickCell
                    }
 
 
--- Nineteen68/plugins/SAP/sap_shell_keywords.py
+++ Nineteen68/plugins/SAP/sap_shell_keywords.py
@@ -233,3 +233,95 @@ class Shell_Keywords():
                 import traceback
                 traceback.print_exc()
             return status,result,value,err_msg
+
+    def clickCell(self, sap_id,url,input_val,*args):
+            self.lk.setWindowToForeground(sap_id)
+            id,ses=self.uk.getSapElement(sap_id)
+            status=sap_constants.TEST_RESULT_FAIL
+            result=sap_constants.TEST_RESULT_FALSE
+            err_msg=None
+            r_int=long(input_val[0])
+            i_int=int(input_val[1])
+            row=r_int-1
+            index=i_int-1
+            value=OUTPUT_CONSTANT
+            elem = ses.FindById(id)
+            d1,d2,imax,d3=self.get_colCount(sap_id)
+            bList=[]
+            try:
+                if(id != None):
+                    if(elem.type == 'GuiShell'):
+                        if elem.rowCount!=0:
+                            #----------------------------------function to append columnorder
+                            for i in range(0,imax-1):
+                                        b=elem.columnorder(i)
+                                        bList.append(b)
+                            #----------------------------------function to append columnorder
+                            if index>=0 and row >=0:
+                                column=str(bList[index])
+                                elem.Click(row,column)
+                                status = sap_constants.TEST_RESULT_PASS
+                                result = sap_constants.TEST_RESULT_TRUE
+                            else:
+                                logger.print_on_console('Row and column values are incorrect')
+                                err_msg = sap_constants.ERROR_MSG
+
+                    else:
+                        logger.print_on_console('Element is not a shell object')
+                        err_msg = sap_constants.ERROR_MSG
+                else:
+                      logger.print_on_console('element not present on the page where operation is trying to be performed')
+                      err_msg = sap_constants.ERROR_MSG
+            except Exception as e:
+                err_msg = sap_constants.ERROR_MSG
+                Exceptions.error(e)
+                logger.print_on_console('Error occured in GetCellText and is a :',e)
+                import traceback
+                traceback.print_exc()
+            return status,result,value,err_msg
+
+    def doubleClickCell(self, sap_id,url,input_val,*args):
+            self.lk.setWindowToForeground(sap_id)
+            id,ses=self.uk.getSapElement(sap_id)
+            status=sap_constants.TEST_RESULT_FAIL
+            result=sap_constants.TEST_RESULT_FALSE
+            err_msg=None
+            r_int=long(input_val[0])
+            i_int=int(input_val[1])
+            row=r_int-1
+            index=i_int-1
+            value=OUTPUT_CONSTANT
+            elem = ses.FindById(id)
+            d1,d2,imax,d3=self.get_colCount(sap_id)
+            bList=[]
+            try:
+                if(id != None):
+                    if(elem.type == 'GuiShell'):
+                        if elem.rowCount!=0:
+                            #----------------------------------function to append columnorder
+                            for i in range(0,imax-1):
+                                        b=elem.columnorder(i)
+                                        bList.append(b)
+                            #----------------------------------function to append columnorder
+                            if index>=0 and row >=0:
+                                column=str(bList[index])
+                                elem.DoubleClick(row,column)
+                                status = sap_constants.TEST_RESULT_PASS
+                                result = sap_constants.TEST_RESULT_TRUE
+                            else:
+                                logger.print_on_console('Row and column values are incorrect')
+                                err_msg = sap_constants.ERROR_MSG
+
+                    else:
+                        logger.print_on_console('Element is not a shell object')
+                        err_msg = sap_constants.ERROR_MSG
+                else:
+                      logger.print_on_console('element not present on the page where operation is trying to be performed')
+                      err_msg = sap_constants.ERROR_MSG
+            except Exception as e:
+                err_msg = sap_constants.ERROR_MSG
+                Exceptions.error(e)
+                logger.print_on_console('Error occured in GetCellText and is a :',e)
+                import traceback
+                traceback.print_exc()
+            return status,result,value,err_msg
