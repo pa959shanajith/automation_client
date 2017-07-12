5109214fd8ef4c257473b0c0f57fd383efef2b40
chethan.singh <chethan.singh@WSLK13FODC6-052.SLKSOFT.COM>
2017-07-12 20:46:38 +0530

    Changes made in path to read the slk.ico file for mac for the error iOS_Generic_UI : Alert message is displayed when "displayVariableValue" keyword is debug/executed

--- Nineteen68/plugins/Generic/pause_display_operation.py
+++ Nineteen68/plugins/Generic/pause_display_operation.py
@@ -90,7 +90,7 @@ class Display(wx.Frame):
         wx.Frame.__init__(self, parent, title="Nineteen68 - DisplayValue",   size=(400, 300),style=wx.STAY_ON_TOP|wx.DEFAULT_FRAME_STYLE & ~ (wx.RESIZE_BORDER |wx.MAXIMIZE_BOX|wx.CLOSE_BOX))
         self.SetBackgroundColour('#e6e7e8')
         self.panel = wx.Panel(self)
-        self.iconpath = os.environ["NINETEEN68_HOME"] + "\\Nineteen68\\plugins\\Core\\Images" + "\\slk.ico"
+        self.iconpath = os.environ["NINETEEN68_HOME"] + "/Nineteen68/plugins/Core/Images" + "/slk.ico"
         self.wicon = wx.Icon(self.iconpath, wx.BITMAP_TYPE_ICO)
         self.SetIcon(self.wicon)
 ##        self.sizer = wx.GridBagSizer(1, 2)
