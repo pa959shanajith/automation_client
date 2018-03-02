5ed8679b1a045684e27e3fdbab68ac5bd1234988
divya.d <divya.d@WSLK13FODC3-07R.SLKSOFT.COM>
2018-03-02 12:55:56 +0530

    >>>Removed changes in Controller wrt getInnerTable Keyword
    (cherry picked from commit 0ec14ac60c823765db5d0390ceff9c449b187c4a)

--- Nineteen68/plugins/Core/controller.py
+++ Nineteen68/plugins/Core/controller.py
@@ -427,15 +427,8 @@ class Controller():
                 if STATIC_NONE in x:
                     x=None
                 else:
-                    if x.find(STATIC_SEPARATOR) != -1:
-                        x = x.replace(STATIC_SEPARATOR,SEMICOLON)
-                    if x.startswith('{') and x.endswith('}'):
-                        vals = self.dynamic_var_handler_obj.get_dynamic_value(x)
-                        if type(vals not in [str, unicode,int]):
-                            x= vals
-                    else:
-                        #To Handle dynamic variables of DB keywords,controller object is sent to dynamicVariableHandler
-                        x=self.dynamic_var_handler_obj.replace_dynamic_variable(x,keyword,self)
+                    #To Handle dynamic variables of DB keywords,controller object is sent to dynamicVariableHandler
+                    x=self.dynamic_var_handler_obj.replace_dynamic_variable(x,keyword,self)
                 inpval.append(x)
         return inpval,ignore_status
 
