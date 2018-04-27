125b66635e4bcf22286b9fbef79f7412dcc28e2c
Sakshi Goyal <sakshi.goyal@slkgroup.com>
2018-04-27 18:49:43 +0530

    Made changes in the logic for finding association links

--- Nineteen68/plugins/AutomatedPathGenerator/CheckPossibleMethods.py
+++ Nineteen68/plugins/AutomatedPathGenerator/CheckPossibleMethods.py
@@ -1,6 +1,7 @@
 import logging
+import logger
 log = logging.getLogger('CheckPossibleMethods.py')
-'''This method will link all the user-defined method call with their method definition.'''
+'''This method will link all the user-defined method calls with their method definition.'''
 def main(PossibleMethods, Classes, VarStorage):
     try:
         Variables = None
@@ -21,118 +22,12 @@ def main(PossibleMethods, Classes, VarStorage):
                         break
                 if isinstance(PresentClass, str):
                     continue
-            else:
+            elif PresentClass != None:
                 PresentClass = Classes[PresentClass]
-            if MethodOrClassCall == 'Method':
-                if PosMethodName == 'this':
-                    for j in PresentClass["constructors"]:
-                        if NoOfArguments == j["NoOfFormalParameters"]:
-                            if NoOfArguments > 0:
-                                Variables = j["Variables"]
-                                y = 0
-                                while(Variables.find(",") != -1):
-                                    x = Variables[:Variables.find(",")]
-                                    if y < len(TypeOfArguments):
-                                        if x[:x.find(
-                                                " ")] == TypeOfArguments[y]:
-                                            y = y + 1
-                                            Variables.replace(
-                                                Variables[:Variables.find(",") + 1], "")
-                                        else:
-                                            break
-                                    else:
-                                        break
-                                if y < len(TypeOfArguments):
-                                    if Variables[:Variables.find(
-                                            " ")] == TypeOfArguments[y]:
-                                        y = y + 1
-                                if y == NoOfArguments:
-                                    PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
-                            else:
-                                PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
-                elif PosMethodName == 'super':
-                    for j in Classes:
-                        if j["name"] == PresentClass["extends"]:
-                            PresentClass = j
-                            if PresentClass["constructors"] is not None:
-                                for j in PresentClass["constructors"]:
-                                    if NoOfArguments == j["NoOfFormalParameters"]:
-                                        if NoOfArguments > 0:
-                                            Variables = j["Variables"]
-                                            y = 0
-                                            while(Variables.find(",") != -1):
-                                                x = Variables[:Variables.find(
-                                                    ",")]
-                                                if y < len(TypeOfArguments):
-                                                    if x[:x.find(
-                                                            " ")] == TypeOfArguments[y]:
-                                                        y = y + 1
-                                                        Variables.replace(
-                                                            Variables[:Variables.find(",") + 1], "")
-                                                    else:
-                                                        break
-                                                else:
-                                                    break
-                                            if y < len(TypeOfArguments):
-                                                if Variables[:Variables.find(
-                                                        " ")] == TypeOfArguments[y]:
-                                                    y = y + 1
-                                            if y == NoOfArguments:
-                                                PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
-                                        else:
-                                            PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
-                elif PresentClass["methods"] is not None:
-                    if PosMethodName in PresentClass["methods"]:
-                        for j in (PresentClass["methods"])[PosMethodName]:
-                            if NoOfArguments == j["NoOfFormalParameters"]:
-                                '''if NoOfArguments > 0:
-                                    Variables = j["Variables"]
-                                    y = 0
-                                    while(Variables.find(",") != -1):
-                                        x = Variables[:Variables.find(",")]
-                                        if y < len(TypeOfArguments):
-                                            if x[:x.find(
-                                                    " ")] == TypeOfArguments[y]:
-                                                y = y + 1
-                                                Variables.replace(
-                                                    Variables[:Variables.find(",") + 1], "")
-                                            else:
-                                                break
-                                        else:
-                                            break
-                                    if y < len(TypeOfArguments):
-                                        if Variables[:Variables.find(
-                                                " ")] == TypeOfArguments[y]:
-                                            y = y + 1
-
-                                    if y == NoOfArguments:
-                                        PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
-                                else:
-                                    PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]'''
-                                PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
-                    else:
-                        for keys in VarStorage:
-                            if (str(keys) in PosMethodName and VarStorage[keys] in classnames):
-                                PossibleMethods[i]["Class"] = VarStorage[keys]
-                                PossibleMethods[i]["MethodOrClassCall"] = "Variable"
-                                break
-                        if(PossibleMethods[i]["MethodOrClassCall"] != "Variable"):
-                            for cls in classnames:
-                                if (str(cls) in PosMethodName):
-                                    PossibleMethods[i]["Class"] = cls
-                                    PossibleMethods[i]["MethodOrClassCall"] = "Variable"
-                                    break
-                while PossibleMethods[i]["ParentNodeNo"] is None and PresentClass["extends"] is not None:
-                    extend = False
-                    for k in Classes:
-                        if k["name"] == PresentClass["extends"]:
-                            PresentClass = k
-                            extend = True
-                            break
-                    if not extend:
-                        break
-                    if PosMethodName in PresentClass["methods"]:
-                        for j in (PresentClass["methods"])[PosMethodName]:
+            if(PresentClass != None):
+                if MethodOrClassCall == 'Method':
+                    if PosMethodName == 'this':
+                        for j in PresentClass["constructors"]:
                             if NoOfArguments == j["NoOfFormalParameters"]:
                                 if NoOfArguments > 0:
                                     Variables = j["Variables"]
@@ -157,11 +52,89 @@ def main(PossibleMethods, Classes, VarStorage):
                                         PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
                                 else:
                                     PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
-            else:
-                for k in Classes:
-                    if k["name"] == PosMethodName:
-                        if ((k["constructors"] is not None) and (k["constructors"] != [])):
-                            for j in k["constructors"]:
+                    elif PosMethodName == 'super':
+                        for j in Classes:
+                            if j["name"] == PresentClass["extends"]:
+                                PresentClass = j
+                                if PresentClass["constructors"] is not None:
+                                    for j in PresentClass["constructors"]:
+                                        if NoOfArguments == j["NoOfFormalParameters"]:
+                                            if NoOfArguments > 0:
+                                                Variables = j["Variables"]
+                                                y = 0
+                                                while(Variables.find(",") != -1):
+                                                    x = Variables[:Variables.find(
+                                                        ",")]
+                                                    if y < len(TypeOfArguments):
+                                                        if x[:x.find(
+                                                                " ")] == TypeOfArguments[y]:
+                                                            y = y + 1
+                                                            Variables.replace(
+                                                                Variables[:Variables.find(",") + 1], "")
+                                                        else:
+                                                            break
+                                                    else:
+                                                        break
+                                                if y < len(TypeOfArguments):
+                                                    if Variables[:Variables.find(
+                                                            " ")] == TypeOfArguments[y]:
+                                                        y = y + 1
+                                                if y == NoOfArguments:
+                                                    PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
+                                            else:
+                                                PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
+                    elif PresentClass["methods"] is not None:
+                        if PosMethodName in PresentClass["methods"]:
+                            for j in (PresentClass["methods"])[PosMethodName]:
+                                if NoOfArguments == j["NoOfFormalParameters"]:
+                                    '''if NoOfArguments > 0:
+                                        Variables = j["Variables"]
+                                        y = 0
+                                        while(Variables.find(",") != -1):
+                                            x = Variables[:Variables.find(",")]
+                                            if y < len(TypeOfArguments):
+                                                if x[:x.find(
+                                                        " ")] == TypeOfArguments[y]:
+                                                    y = y + 1
+                                                    Variables.replace(
+                                                        Variables[:Variables.find(",") + 1], "")
+                                                else:
+                                                    break
+                                            else:
+                                                break
+                                        if y < len(TypeOfArguments):
+                                            if Variables[:Variables.find(
+                                                    " ")] == TypeOfArguments[y]:
+                                                y = y + 1
+
+                                        if y == NoOfArguments:
+                                            PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
+                                    else:
+                                        PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]'''
+                                    PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
+                        else:
+                            for keys in VarStorage:
+                                if (str(keys) in PosMethodName and VarStorage[keys] in classnames):
+                                    PossibleMethods[i]["Class"] = VarStorage[keys]
+                                    PossibleMethods[i]["MethodOrClassCall"] = "Variable"
+                                    break
+                            if(PossibleMethods[i]["MethodOrClassCall"] != "Variable"):
+                                for cls in classnames:
+                                    if (str(cls) in PosMethodName):
+                                        PossibleMethods[i]["Class"] = cls
+                                        PossibleMethods[i]["MethodOrClassCall"] = "Variable"
+                                        break
+                    while PossibleMethods[i]["ParentNodeNo"] is None and PresentClass["extends"] is not None:
+                        extend = False
+                        for k in Classes:
+                            if k["name"] == PresentClass["extends"]:
+                                PresentClass = k
+                                extend = True
+                                break
+                        if not extend:
+                            break
+                        if PosMethodName in PresentClass["methods"]:
+                            for j in (PresentClass["methods"])[PosMethodName]:
                                 if NoOfArguments == j["NoOfFormalParameters"]:
                                     if NoOfArguments > 0:
                                         Variables = j["Variables"]
@@ -186,8 +159,40 @@ def main(PossibleMethods, Classes, VarStorage):
                                             PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
                                     else:
                                         PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
-                        #else:
-                         #   PossibleMethods[i]["ParentNodeNo"] = k["position"]
+                else:
+                    for k in Classes:
+                        if k["name"] == PosMethodName:
+                            if ((k["constructors"] is not None) and (k["constructors"] != [])):
+                                for j in k["constructors"]:
+                                    if NoOfArguments == j["NoOfFormalParameters"]:
+                                        '''if NoOfArguments > 0:
+                                            Variables = j["Variables"]
+                                            y = 0
+                                            while(Variables.find(",") != -1):
+                                                x = Variables[:Variables.find(",")]
+                                                if y < len(TypeOfArguments):
+                                                    if x[:x.find(
+                                                            " ")] == TypeOfArguments[y]:
+                                                        y = y + 1
+                                                        Variables.replace(
+                                                            Variables[:Variables.find(",") + 1], "")
+                                                    else:
+                                                        break
+                                                else:
+                                                    break
+                                            if y < len(TypeOfArguments):
+                                                if Variables[:Variables.find(
+                                                        " ")] == TypeOfArguments[y]:
+                                                    y = y + 1
+                                            if y == NoOfArguments:
+                                                PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
+                                        else:
+                                            PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]'''
+                                        PossibleMethods[i]["ParentNodeNo"] = j["NodeNo"]
+                            else:
+                                PossibleMethods[i]["ParentNodeNo"] = k["position"]
     except Exception as e:
+        import traceback
+        print (traceback.format_exc())
         log.error(e)
     return PossibleMethods
