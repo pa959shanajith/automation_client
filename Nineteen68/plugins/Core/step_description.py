6826043ddf5b9e7173595baa4284bba2fdeb0d4c
divya.d <divya.d@WSLK13FODC6-43R.SLKSOFT.COM>
2018-05-10 16:40:11 +0530

    >>> Issue :Changes in step description

--- Nineteen68/plugins/Core/step_description.py
+++ Nineteen68/plugins/Core/step_description.py
@@ -13,13 +13,9 @@ class StepDescription:
 
 
     def generic(self,keyword,tsp,inputval,input,output,con,reporting_obj):
-        print output
-
         if str(keyword) in ["typecast","split","displayvariablevalue"]:
             if type(output) is list:
                     output=','.join(output)
-                    print output
-
         #Date operations
         def getcurrenttime():
             return 'Get current time of the system and save the time '+ "'"+ output + "'"+ ' in '+ "'"+ tsp.outputval+ "'"
@@ -1122,7 +1118,7 @@ class StepDescription:
             if tsp.custname=="@Custom":
                 return 'Select value '+"'" +input+"'" +' with visible text '+"'" +visible_text+"'" +' of the type '+"'" +ele_type+"'" +' with the index '+"'" +cust_index+"'" +' present in '+ "'" + tsp.custname + "'"
             else:
-                return ' Select value with index value '+"'"+ input+"'"+' in the '+"'" + tsp.custname + "'"
+                return 'Select the value '+output+' with the index '+input+' present in the table cell '+"'" + tsp.custname+'['+inputval[0]+']['+inputval[1]+']'+"'."
         def getcount():
             return 'Get the count of values in the '+ "'" + tsp.custname + "'"+ ' and save the count ' +"'"+output+"'"+' in '+"'"+tsp.outputval+"'"
         #Radio checkbox keywords
@@ -1182,7 +1178,10 @@ class StepDescription:
         def clickelement():
             return ' Click '+ "'" + tsp.custname + "'"
         def mouseclick():
-            return ' Mouse Click on  '+ "'" + tsp.custname + "'"
+            if len(inputval)>0:
+                return ' Mouse Click on  '+ "'" + tsp.custname +'['+inputval[0]+']['+inputval[1]+']'+"'"
+            else:
+                return ' Mouse Click on  '+ "'" + tsp.custname + "'"
         def verifyelementtext():
             return 'Verify ' +"'"+ input +"'"+ ' is the text of the '+ "'" + tsp.custname + "'"
         def verifyelementexists():
@@ -1251,7 +1250,9 @@ class StepDescription:
 
         #Table keywords
         def getcelltooltip():
-            return 'Get the cell tooltip from the '+ "'" + tsp.custname + "'"+ ' and save the tool tip text ' +"'"+output+"'"+' in ' +"'"+tsp.outputval+"'"
+            return 'Get the cell tooltip from the '+ "'" + tsp.custname +'['+inputval[0]+']['+inputval[1]+']'+ "'"+ ' and save the tool tip text ' +"'"+output+"'"+' in ' +"'"+tsp.outputval+"'"
+        def verifycelltooltip():
+            return 'Verify cell tooltip value '+"'"+input+"'"+' is present in the '+ "'"+ tsp.custname +'['+inputval[0]+']['+inputval[1]+']'+ "' table."
         def cellclick():
             return 'Click ' + "'" + tsp.custname + "'"
         def getrowcount():
@@ -1259,15 +1260,17 @@ class StepDescription:
         def getcolumncount():
             return 'Get column count of the '+ "'" + tsp.custname + "'"+ ' and save the count '+"'"+output+"'"+' in '+"'"+tsp.outputval+"'"
         def verifycellvalue():
-            return 'Verify cell value [Null] is present in the '+ "'" + tsp.custname + "'" + ' Invalid input'
+            input=input.split(',')#making list of the input string
+            input=input[2:]# getting list elements from pos 3 till end
+            input=','.join(input)# if list is make it into a string
+            return 'Verify cell value'+"'"+str(input)+"'" +'  is present in the '+ "'" + tsp.custname +'['+inputval[0]+']['+inputval[1]+']'+"' table."
 
         def getcolnumbytext():
             return 'Get column number of ' + "'" + tsp.custname + "'" + ' by text '+"'"+input +"'"+' and save the column number ' +"'"+ output + "'"+' in '+"'"+tsp.outputval+"'"
         def getrownumbytext():
             return 'Get row number of ' + "'" + tsp.custname + "'" + ' by text '+"'"+input +"'"+' and save the row number ' +"'"+output +"'"+ ' in '+"'"+tsp.outputval+"'"
         def getcellvalue():
-            return 'Get row number of ' + "'" + tsp.custname + "'" + ' by text '+"'"+input +"'"+' and save the row number ' +"'"+output +"'"+ ' in '+"'"+tsp.outputval+"'"
-
+            return 'Get cell value of ' + "'" + tsp.custname +'['+inputval[0]+']['+inputval[1]+']'+"'" + ' in the table and save the value ' +"'"+output +"'"+ ' in '+"'"+tsp.outputval+"'"
         #custom keyword
         def getobjectcount():
             return 'Get Object count of the type '+"'" +ele_type+"'" +' and save the count '+"'" +output+"'" +' in '+"'"+tsp.outputval+"'"
