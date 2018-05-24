bc42b064b9ebc13bc6c6681005e55bec6576bbd2
anas.ahmed <anas.ahmed@WSLK13FODC6-050.SLKSOFT.COM>
2018-05-24 11:29:30 +0530

    #1845-Prod_MNT : Outlook: For few mails, if From, To and Subject is same then latest occurrence of the mail is not getting #1822-Production Changes need to merge in master of Outlook Keyword

--- Nineteen68/plugins/Desktop/outlook.py
+++ Nineteen68/plugins/Desktop/outlook.py
@@ -2,10 +2,10 @@
 # Name:        outlook.py
 # Purpose:     To automate 'Outlook'
 #
-# Author:      prudhvi.gujjuboyina,anas.ahmed
+# Author:      prudhvi.gujjuboyina, anas.ahmed
 #
 # Created:     07-09-2016
-# Copyright:   (c) prudhvi.gujjuboyina 2016,(c) anas.ahmed 2018
+# Copyright:   (c) prudhvi.gujjuboyina 2016, (c) anas.ahmed 2018
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
@@ -122,11 +122,11 @@ class OutlookKeywords:
                 traceback.print_exc()
                 log.error(e)
                 logger.print_on_console('Check the given path')
+                error_msg="ERROR OCCURED ",e
             return status,method_output,result,error_msg
 
 
-        """  This method takes 3 Input Params :1.Sender Name (Display name  in Outlook application) 2.Subject  3.To whom mail has been sent
-On success return the mail content in string format else return fail"""
+        """  This method takes 3 Input Params :1.Sender Name (Display name  in Outlook application) 2.Subject  3.To whom mail has been sent On success return the mail content in string format else return fail"""
         def GetEmail(self,input,*args):
             try:
                 status=desktop_constants.TEST_RESULT_FAIL
@@ -153,52 +153,38 @@ On success return the mail content in string format else return fail"""
                     inbox = self.outlook.GetDefaultFolder(outlook_constants.INBOX_FOLDER)
                 all_inbox = inbox.Items
                 folders = inbox.Folders
-                all_inbox=reversed(list(all_inbox))
-                for msg in all_inbox:
-                    if msg.Class==43:
-                        if msg.SenderName==self.senderEmail:
-                            if msg.Subject==self.subject:
-                                tomails = msg.To
-                                tomailsList = tomails.split(';')
-                                for to in tomailsList:
-                                    to = str(to)
-                                    to = to.strip()
-                                    if self.toMail == to:
-                                        if msg.SenderEmailType=='EX':
-                                            try:
-                                                FromMailId= msg.Sender.PropertyAccessor.GetProperty(outlook_constants.PR_SMTP_ADDRESS)
-                                            except:
-                                                pass
-                                        else:
-                                            FromMailId=msg.SenderEmailAddress
-                                        for recipient in  msg.Recipients:
-                                            if recipient.Type==1:
-                                                ToMailID+=recipient.PropertyAccessor.GetProperty(outlook_constants.PR_SMTP_ADDRESS)+';'
-                                        ToMailID=ToMailID[:-1]
-                                        if msg.Attachments.Count>0:
-                                            AttachmentStatus=outlook_constants.ATTACH_STATUS_YES
-                                        try:
-                                                msg.Display()
-                                                Flag=True
-                                                key=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5)) #creating a random key of length '5'
-                                                self.GetEmailList.append({'Subject':msg.Subject,'ToMailID':ToMailID,'FromMailId':FromMailId,'AttachmentStatus':AttachmentStatus,'Body':msg.Body,'Flag':Flag,'Key':key,'Content':msg})
-                                                result=key
-                                                status=desktop_constants.TEST_RESULT_PASS
-                                                method_output=desktop_constants.TEST_RESULT_TRUE
-                                        except Exception as e:# done to handle popups and dialog boxes
-                                            f=e[2]
-                                            if str(f[2])=="Outlook can't do this because a dialog box is open. Please close it and try again.":
-                                                logger.print_on_console(str(f[2]))
-                                                error_msg=str(f[2])
-                                        break
-                                    else:
-                                        continue
-                            else:
-                                continue
-                            #added this break statement to stop opening multiple emails of the same input types (from,to,subject)
-                            break
-                        else :
-                            continue
+                #all_inbox=reversed(list(all_inbox))
+                #-------------fetching sorted mails and storing in list variable mails
+                mails=self.messageSegregator(all_inbox,self.toMail,self.senderEmail,self.subject)
+                msg=mails[0] # as it is the latest msg object by date/time.
+                #----------------------------------------------------------------------
+                if msg.SenderEmailType=='EX':
+                    try:
+                        FromMailId= msg.Sender.PropertyAccessor.GetProperty(outlook_constants.PR_SMTP_ADDRESS)
+                    except:
+                        pass
+                else:
+                    FromMailId=msg.SenderEmailAddress
+                for recipient in  msg.Recipients:
+                    if recipient.Type==1:
+                        ToMailID+=recipient.PropertyAccessor.GetProperty(outlook_constants.PR_SMTP_ADDRESS)+';'
+                ToMailID=ToMailID[:-1]
+                if msg.Attachments.Count>0:
+                    AttachmentStatus=outlook_constants.ATTACH_STATUS_YES
+                try:
+                        msg.Display()
+                        Flag=True
+                        key=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5)) #creating a random key of length '5'
+                        self.GetEmailList.append({'Subject':msg.Subject,'ToMailID':ToMailID,'FromMailId':FromMailId,'AttachmentStatus':AttachmentStatus,'Body':msg.Body,'Flag':Flag,'Key':key,'Content':msg})
+                        result=key
+                        status=desktop_constants.TEST_RESULT_PASS
+                        method_output=desktop_constants.TEST_RESULT_TRUE
+                except Exception as e:# done to handle popups and dialog boxes
+                    f=e[2]
+                    if str(f[2])=="Outlook can't do this because a dialog box is open. Please close it and try again.":
+                        logger.print_on_console(str(f[2]))
+                        error_msg=str(f[2])
+
                 if Flag!=True:
                     if error_msg==None:
                         logger.print_on_console('Error: No such mail found')
@@ -208,6 +194,7 @@ On success return the mail content in string format else return fail"""
                 traceback.print_exc()
                 log.error(e)
                 logger.print_on_console('Error: No such mail found')
+                error_msg="ERROR OCCURED ",e
             return status,method_output,result,error_msg
 
         def GetFromMailId(self,input,*args):
@@ -231,6 +218,7 @@ On success return the mail content in string format else return fail"""
                 traceback.print_exc()
                 log.error(e)
                 logger.print_on_console('Error : No such mail id found')
+                error_msg="ERROR OCCURED ",e
             return status,method_output,res,error_msg
 
 
@@ -263,6 +251,7 @@ On success return the mail content in string format else return fail"""
                 traceback.print_exc()
                 log.error(e)
                 logger.print_on_console('Error : mail does''t have such info')
+                error_msg="ERROR OCCURED ",e
             return status,method_output,res,error_msg
 
         def GetSubject(self,input,*args):
@@ -286,6 +275,7 @@ On success return the mail content in string format else return fail"""
                 traceback.print_exc()
                 log.error(e)
                 logger.print_on_console('Error : No subject found')
+                error_msg="ERROR OCCURED ",e
             return status,method_output,res,error_msg
 
         def GetToMailID(self,input,*args):
@@ -309,6 +299,7 @@ On success return the mail content in string format else return fail"""
                 traceback.print_exc()
                 log.error(e)
                 logger.print_on_console('Error : No such mail id found')
+                error_msg="ERROR OCCURED ",e
             return status,method_output,res,error_msg
 
 
@@ -335,6 +326,7 @@ On success return the mail content in string format else return fail"""
                     traceback.print_exc()
                     log.error(e)
                     logger.print_on_console('Error : No Body found')
+                    error_msg="ERROR OCCURED ",e
             return status,method_output,res,error_msg
 
 
@@ -359,10 +351,12 @@ On success return the mail content in string format else return fail"""
             except Exception as  e:
                 import traceback
                 traceback.print_exc()
+                log.error(e)
                 logger.print_on_console('Wrong input given')
+                error_msg="ERROR OCCURED ",e
             return status,method_output,res,error_msg
 
-# Internal method to search for a folder in given folder
+        """Internal method to search for a folder in given folder"""
         def findFolder(self,folderName,searchIn):
             try:
                 lowerAccount = searchIn.Folders
@@ -377,6 +371,63 @@ On success return the mail content in string format else return fail"""
             except Exception as error:
                 logger.print_on_console( "Looks like we had an issue accessing the searchIn object")
                 return None
+
+        """Returns a list of segregated msg com-objects"""
+        """This method might return a delayed response as it has to iterate through all objects of the msg folder"""
+        """Input : msg_objects[]; 'To' Address; 'Sender' Address; Subject'"""
+        """Output: List of segregated message objects 'segregatedList' """
+        """Reason for using this method, was because different versions of outlook ,return msg objects in different order.This method segregates the required messages and arranges them to get the last recieved mesage"""
+        def messageSegregator(self,all_inbox_msgs,To_add,From_add,sub):
+            segregatedList=[]
+            try:
+                """To filtering msg objects by 'Class','SenderName' and 'Subject' address """
+                """Returns a List of msg objects"""
+                def mailSegregator(all_inbox_msgs,To_add,From_add,sub):
+                    mSegList=[]
+                    try:
+                        #all_inbox_msgs.Item(1).RecievedTime>all_inbox_msgs.Item(all_inbox_msgs.Count).RecieveTime
+                        for message in all_inbox_msgs:
+                            if message.Class==43 and message.SenderName.strip()==From_add.strip() and message.Subject.strip()==sub.strip():
+                                ToMail= message.To
+                                tomailsList=ToMail.split(';')
+                                for to in tomailsList:
+                                    if str(to).strip()==To_add.strip():
+                                        mSegList.append(message)
+                                        break
+                    except Exception as e:
+                        import traceback
+                        traceback.print_exc()
+                        print "Error at mailSegregator",e
+                    return mSegList
+                """To segregate msg objects by 'To' address and then by day/date/time """
+                """Returns a List of msg objects"""
+                def timeSegregator(mailList):
+                    times=[]
+                    sortedList=[]
+                    try:
+                        for i in range(0,len(mailList)):
+                            times.append(mailList[i].CreationTime)
+                        sortedTime=sorted(times, reverse=True)
+                        for i in range(0,len(sortedTime)):
+                            for msg in mailList:
+                                if msg.CreationTime==sortedTime[i]:
+                                    sortedList.append(msg)
+                                    break
+                    except Exception as e:
+                        import traceback
+                        traceback.print_exc()
+                        print "Error in timeSegregator",e
+                    return sortedList
+
+                mailSegList=mailSegregator(all_inbox_msgs,To_add,From_add,sub)
+                segregatedList=timeSegregator(mailSegList)
+
+            except Exception as e:
+                import traceback
+                traceback.print_exc()
+                logger.print_on_console('Error at mail segregator')
+
+            return segregatedList
 
 #------------------------------------------------------------------------------------------------------------Send email function
 
@@ -405,9 +456,9 @@ On success return the mail content in string format else return fail"""
                     status=desktop_constants.TEST_RESULT_PASS
                     method_output=desktop_constants.TEST_RESULT_TRUE
                 except Exception as e:
-                    error_msg="ERROR OCCURED "
-                    #import traceback
-                    #traceback.print_exc()
+                    error_msg="ERROR OCCURED ",e
+                    import traceback
+                    traceback.print_exc()
             else:
                 error_msg="'To...' field is not set"
                 logger.print_on_console(error_msg)
@@ -431,9 +482,9 @@ On success return the mail content in string format else return fail"""
                         status=desktop_constants.TEST_RESULT_PASS
                         method_output=desktop_constants.TEST_RESULT_TRUE
                     except Exception as e:
-                        error_msg="ERROR OCCURED "
-                        #import traceback
-                        #traceback.print_exc()
+                        error_msg="ERROR OCCURED ",e
+                        import traceback
+                        traceback.print_exc()
                 else:
                     error_msg="Please set CC"
             else:
@@ -458,9 +509,9 @@ On success return the mail content in string format else return fail"""
                         status=desktop_constants.TEST_RESULT_PASS
                         method_output=desktop_constants.TEST_RESULT_TRUE
                     except Exception as e:
-                        error_msg="ERROR OCCURED "
-                        #import traceback
-                        #traceback.print_exc()
+                        error_msg="ERROR OCCURED ",e
+                        import traceback
+                        traceback.print_exc()
                 else:
                     error_msg="Please set BCC"
             else:
@@ -486,9 +537,9 @@ On success return the mail content in string format else return fail"""
                         status=desktop_constants.TEST_RESULT_PASS
                         method_output=desktop_constants.TEST_RESULT_TRUE
                     except Exception as e:
-                        error_msg="ERROR OCCURED "
-                        #import traceback
-                        #traceback.print_exc()
+                        error_msg="ERROR OCCURED ",e
+                        import traceback
+                        traceback.print_exc()
                 else:
                     error_msg="Please set the subject of the mail"
             else:
@@ -510,9 +561,9 @@ On success return the mail content in string format else return fail"""
                         status=desktop_constants.TEST_RESULT_PASS
                         method_output=desktop_constants.TEST_RESULT_TRUE
                     except Exception as e:
-                        error_msg="ERROR OCCURED "
-                        #import traceback
-                        #traceback.print_exc()
+                        error_msg="ERROR OCCURED ",e
+                        import traceback
+                        traceback.print_exc()
                 else:
                     error_msg="Please set the body of the mail"
             else:
@@ -539,9 +590,9 @@ On success return the mail content in string format else return fail"""
                             status=desktop_constants.TEST_RESULT_PASS
                             method_output=desktop_constants.TEST_RESULT_TRUE
                     except Exception as e:
-                        error_msg="ERROR OCCURED "
-                        #import traceback
-                        #traceback.print_exc()
+                        error_msg="ERROR OCCURED ",e
+                        import traceback
+                        traceback.print_exc()
                 else:
                     error_msg="Please set the attachment path"
             else:
@@ -575,9 +626,9 @@ On success return the mail content in string format else return fail"""
                         status=desktop_constants.TEST_RESULT_PASS
                         method_output=desktop_constants.TEST_RESULT_TRUE
                 except Exception as e:
-                    error_msg="ERROR OCCURED "
-                    #import traceback
-                    #traceback.print_exc()
+                    error_msg="ERROR OCCURED ",e
+                    import traceback
+                    traceback.print_exc()
             else:
                 error_msg="Please set the to mail ID"
             return status,method_output,result,error_msg
