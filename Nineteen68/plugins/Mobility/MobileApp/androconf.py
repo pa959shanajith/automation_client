6e7cd34fc9f87e698facf6315c0962c4fe19e65d
sushma.p <sushma.p@slkgroup.com>
2019-02-01 13:12:28 +0530

    Resolved type error long

--- Nineteen68/plugins/Mobility/MobileApp/androconf.py
+++ Nineteen68/plugins/Mobility/MobileApp/androconf.py
@@ -171,24 +171,24 @@ def long2int(l):
 
 def long2str(l):
     """Convert an integer to a string."""
-    if type(l) not in (types.IntType, types.LongType):
+    if type(l) not in (int):
         raise ValueError('the input must be an integer')
 
     if l < 0:
         raise ValueError('the input must be greater than 0')
     s = ''
     while l:
-        s = s + chr(l & 255L)
+        s = s + chr(l & 255)
         l >>= 8
 
     return s
 
 def str2long(s):
     """Convert a string to a long integer."""
-    if type(s) not in (types.StringType, types.UnicodeType):
+    if type(s) not in (bytes, str):
         raise ValueError('the input must be a string')
 
-    l = 0L
+    l = 0
     for i in s:
         l <<= 8
         l |= ord(i)
