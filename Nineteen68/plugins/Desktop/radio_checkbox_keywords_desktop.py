cee11ff8386406a7c7a4b88765d1af5780ded78a
Anas Ahmed <anas.ahmed@SLKSOFT.COM>
2019-04-30 10:46:24 +0530

    #2404-Desktop: Radio button and checkbox object type's keywords issue (Method B)

--- Nineteen68/plugins/Desktop/radio_checkbox_keywords_desktop.py
+++ Nineteen68/plugins/Desktop/radio_checkbox_keywords_desktop.py
@@ -1,8 +1,8 @@
 #-------------------------------------------------------------------------------
 # Name:        radio_checkbox_keywords_desktop.py
-# Purpose:     Radio check box operations
+# Purpose:     Radio button/check box operations
 #
-# Author:      wasimakram.sutar
+# Author:      wasimakram.sutar,anas.ahmed
 #
 # Created:     09-05-2017
 # Copyright:   (c) wasimakram.sutar 2017
@@ -17,7 +17,7 @@ import logging
 
 log = logging.getLogger('radio_checkbox_keywords_desktop.py')
 class Radio_Checkbox_keywords():
-    def select_radiobutton(self, element , parent  , *args):
+    def select_radiobutton(self,element ,parent ,*args):
         status=desktop_constants.TEST_RESULT_FAIL
         result=desktop_constants.TEST_RESULT_FALSE
         verb = OUTPUT_CONSTANT
@@ -32,17 +32,19 @@ class Radio_Checkbox_keywords():
                 log.debug(parent)
                 log.debug('Parent check status')
                 log.debug(check)
-                if (check):
+                if ( check ):
                     log.info('Parent matched')
-                    if(element.is_enabled()):
+                    if( element.is_enabled() ):
                         state = self.get_status(element,parent)
-                        if state[2] == 'Selected':
-                            log.info( 'Radio button already selected')
+                        if ( state[2] == 'Selected' ):
                             err_msg='Radio button already selected'
-                            logger.print_on_console('Radio button already selected')
-
+                            log.info( err_msg )
+                            logger.print_on_console( err_msg )
                         else:
-                            element.check_by_click()
+                            if ( element.backend.name=='win32' ):
+                                element.check_by_click()
+                            elif ( element.backend.name=='uia' ):
+                                element.select()
                             status = desktop_constants.TEST_RESULT_PASS
                             result = desktop_constants.TEST_RESULT_TRUE
                             log.info(STATUS_METHODOUTPUT_UPDATE)
@@ -50,20 +52,20 @@ class Radio_Checkbox_keywords():
                         result = desktop_constants.TEST_RESULT_TRUE
                         log.info(STATUS_METHODOUTPUT_UPDATE)
                     else:
-                      log.info('Element state does not allow to perform the operation')
-                      logger.print_on_console('Element state does not allow to perform the operation')
-                      err_msg= 'Element state does not allow to perform the operation'
+                        err_msg= 'Element state does not allow to perform the operation'
+                        log.info( err_msg )
+                        logger.print_on_console( err_msg )
                 else:
-                   log.info('Element not present on the page where operation is trying to be performed')
-                   err_msg='Element not present on the page where operation is trying to be performed'
-                   logger.print_on_console('Element not present on the page where operation is trying to be performed')
+                    err_msg='Element not present on the page where operation is trying to be performed'
+                    log.info( err_msg )
+                    logger.print_on_console( err_msg )
         except Exception as exception:
             log.error(exception)
             logger.print_on_console(exception)
             err_msg = desktop_constants.ERROR_MSG
         return status,result,verb,err_msg
 
-    def select_checkbox(self, element , parent  , *args):
+    def select_checkbox(self,element ,parent ,*args):
         status=desktop_constants.TEST_RESULT_FAIL
         result=desktop_constants.TEST_RESULT_FALSE
         verb = OUTPUT_CONSTANT
@@ -78,35 +80,38 @@ class Radio_Checkbox_keywords():
                 log.debug(parent)
                 log.debug('Parent check status')
                 log.debug(check)
-                if (check):
+                if ( check ):
                     log.info('Parent matched')
-                    if(element.is_enabled()):
+                    if( element.is_enabled() ):
                         state = self.get_status(element,parent)
-                        if state[2] == 'Checked':
-                            log.info( 'Check box is already checked')
+                        if ( state[2] == 'Checked' ):
                             err_msg='Check box is already checked'
-                            logger.print_on_console('Check box is already checked')
+                            log.info( err_msg )
+                            logger.print_on_console( err_msg )
                         else:
-                            element.check()
+                            if ( element.backend.name=='win32' ):
+                                element.check()
+                            elif ( element.backend.name=='uia' ):
+                                element.toggle()
                             status = desktop_constants.TEST_RESULT_PASS
                             result = desktop_constants.TEST_RESULT_TRUE
                             log.info(STATUS_METHODOUTPUT_UPDATE)
                             log.info(STATUS_METHODOUTPUT_UPDATE)
                     else:
-                      log.info('Element state does not allow to perform the operation')
-                      logger.print_on_console('Element state does not allow to perform the operation')
-                      err_msg= 'Element state does not allow to perform the operation'
+                        err_msg= 'Element state does not allow to perform the operation'
+                        log.info( err_msg )
+                        logger.print_on_console( err_msg )
                 else:
-                   log.info('Element not present on the page where operation is trying to be performed')
-                   err_msg='Element not present on the page where operation is trying to be performed'
-                   logger.print_on_console('Element not present on the page where operation is trying to be performed')
+                    err_msg='Element not present on the page where operation is trying to be performed'
+                    log.info( err_msg )
+                    logger.print_on_console( err_msg )
         except Exception as exception:
-            log.error(exception)
-            logger.print_on_console(exception)
+            log.error( exception )
+            logger.print_on_console( exception )
             err_msg = desktop_constants.ERROR_MSG
         return status,result,verb,err_msg
 
-    def unselect_checkbox(self, element , parent  , *args):
+    def unselect_checkbox(self,element ,parent ,*args):
         status=desktop_constants.TEST_RESULT_FAIL
         result=desktop_constants.TEST_RESULT_FALSE
         verb = OUTPUT_CONSTANT
@@ -121,47 +126,56 @@ class Radio_Checkbox_keywords():
                 log.debug(parent)
                 log.debug('Parent check status')
                 log.debug(check)
-                if (check):
+                if ( check ):
                     log.info('Parent matched')
-                    if(element.is_enabled()):
+                    if( element.is_enabled() ):
                         state = self.get_status(element,parent)
-                        if state[2] == 'UnChecked':
-                            log.info( 'Check box is already unchecked')
+                        if ( state[2] == 'UnChecked' ):
                             err_msg='Check box is already unchecked'
-                            logger.print_on_console('Check box is already unchecked')
+                            log.info(err_msg)
+                            logger.print_on_console(err_msg)
                         else:
-                            element.uncheck()
+                            if ( element.backend.name=='win32' ):
+                                element.uncheck()
+                            elif ( element.backend.name=='uia' ):
+                                element.toggle()
                             status = desktop_constants.TEST_RESULT_PASS
                             result = desktop_constants.TEST_RESULT_TRUE
                             log.info(STATUS_METHODOUTPUT_UPDATE)
                     else:
-                      log.info('Element state does not allow to perform the operation')
-                      logger.print_on_console('Element state does not allow to perform the operation')
-                      err_msg= 'Element state does not allow to perform the operation'
+                        err_msg= 'Element state does not allow to perform the operation'
+                        log.info( err_msg )
+                        logger.print_on_console( err_msg )
                 else:
-                   log.info('Element not present on the page where operation is trying to be performed')
-                   err_msg='Element not present on the page where operation is trying to be performed'
-                   logger.print_on_console('Element not present on the page where operation is trying to be performed')
+                    err_msg='Element not present on the page where operation is trying to be performed'
+                    log.info( err_msg )
+                    logger.print_on_console( err_msg )
         except Exception as exception:
-            log.error(exception)
-            logger.print_on_console(exception)
+            log.error( exception )
+            logger.print_on_console( exception )
             err_msg = desktop_constants.ERROR_MSG
         return status,result,verb,err_msg
 
-    def get_status(self, element , parent  , *args):
+    def get_status(self,element ,parent ,*args):
         status=desktop_constants.TEST_RESULT_FAIL
         result=desktop_constants.TEST_RESULT_FALSE
         flag=None
         err_msg=None
         try:
-            if desktop_launch_keywords.window_name!=None:
+            if ( desktop_launch_keywords.window_name!=None ):
                 verify_obj = Text_Box()
                 check = verify_obj.verify_parent(element,parent)
-                if (check):
+                if ( check ):
                     tag = element.friendly_class_name()
-                    status = element.get_check_state()
-                    if(tag == 'CheckBox'):
-                        if(status == 1):
+                    if ( element.backend.name=='uia' ):
+                        if ( tag=='RadioButton' ):
+                            status = element.is_selected()
+                        elif ( tag=='CheckBox' ):
+                            status = element.get_toggle_state()
+                    else:
+                        status = element.get_check_state()
+                    if( tag == 'CheckBox' ):
+                        if( status == 1 ):
                             flag = 'Checked'
                             status=desktop_constants.TEST_RESULT_PASS
                             result = desktop_constants.TEST_RESULT_TRUE
@@ -169,8 +183,8 @@ class Radio_Checkbox_keywords():
                             flag = 'UnChecked'
                             status=desktop_constants.TEST_RESULT_PASS
                             result = desktop_constants.TEST_RESULT_TRUE
-                    if(tag == 'RadioButton'):
-                        if(status == 1):
+                    if( tag == 'RadioButton' ):
+                        if( status == 1 ):
                             flag = 'Selected'
                             status=desktop_constants.TEST_RESULT_PASS
                             result = desktop_constants.TEST_RESULT_TRUE
@@ -178,18 +192,16 @@ class Radio_Checkbox_keywords():
                             flag = 'UnSelected'
                             status=desktop_constants.TEST_RESULT_PASS
                             result = desktop_constants.TEST_RESULT_TRUE
-                    elif(tag == 'Button'):
+                    elif( tag == 'Button' ):
                         flag = status
                         status=desktop_constants.TEST_RESULT_PASS
                         result = desktop_constants.TEST_RESULT_TRUE
-
                 else:
-                   log.info('Element not present on the page where operation is trying to be performed')
-                   err_msg='Element not present on the page where operation is trying to be performed'
-                   logger.print_on_console('Element not present on the page where operation is trying to be performed')
-
+                    err_msg='Element not present on the page where operation is trying to be performed'
+                    log.info( err_msg )
+                    logger.print_on_console( err_msg )
         except Exception as exception:
-            import traceback
-            traceback.print_exc()
+            log.error( exception )
+            logger.print_on_console( exception )
             err_msg = desktop_constants.ERROR_MSG
         return status,result,flag,err_msg
