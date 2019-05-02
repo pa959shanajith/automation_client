26dd87f9a2e09de0ba666c11a33f92fe41507fdc
Anas Ahmed <anas.ahmed@SLKSOFT.COM>
2019-05-02 11:57:30 +0530

    #2404 minor code changes as per standerd/requested

--- Nineteen68/plugins/Desktop/radio_checkbox_keywords_desktop.py
+++ Nineteen68/plugins/Desktop/radio_checkbox_keywords_desktop.py
@@ -23,7 +23,7 @@ class Radio_Checkbox_keywords():
         verb = OUTPUT_CONSTANT
         err_msg=None
         try:
-            if ( desktop_launch_keywords.window_name!=None ):
+            if ( desktop_launch_keywords.window_name ):
                 log.info('Recieved element from the desktop dispatcher')
                 dektop_element = element
                 verify_obj = Text_Box()
@@ -50,9 +50,10 @@ class Radio_Checkbox_keywords():
                         err_msg= 'Element state does not allow to perform the operation'
                 else:
                     err_msg='Element not present on the page where operation is trying to be performed'
-            if ( err_msg!=None ):
+            else:
+                err_msg = 'Unable to find the window in which the element needs to be automated'
+            if ( err_msg ):
                 log.info( err_msg )
-                log.error( err_msg )
                 logger.print_on_console( err_msg )
         except Exception as exception:
             err_msg = desktop_constants.ERROR_MSG
@@ -64,9 +65,9 @@ class Radio_Checkbox_keywords():
         status=desktop_constants.TEST_RESULT_FAIL
         result=desktop_constants.TEST_RESULT_FALSE
         verb = OUTPUT_CONSTANT
-        err_msg=None
+        err_msg = None
         try:
-            if ( desktop_launch_keywords.window_name!=None ):
+            if ( desktop_launch_keywords.window_name ):
                 log.info('Recieved element from the desktop dispatcher')
                 dektop_element = element
                 verify_obj = Text_Box()
@@ -77,14 +78,14 @@ class Radio_Checkbox_keywords():
                 log.debug(check)
                 if ( check ):
                     log.info('Parent matched')
-                    if( element.is_enabled() ):
+                    if ( element.is_enabled() ):
                         state = self.get_status(element,parent)
                         if ( state[2] == 'Checked' ):
                             err_msg='Check box is already checked'
                         else:
-                            if ( element.backend.name=='win32' ):
+                            if ( element.backend.name == 'win32' ):
                                 element.check()
-                            elif ( element.backend.name=='uia' ):
+                            elif ( element.backend.name == 'uia' ):
                                 element.toggle()
                             status = desktop_constants.TEST_RESULT_PASS
                             result = desktop_constants.TEST_RESULT_TRUE
@@ -92,10 +93,11 @@ class Radio_Checkbox_keywords():
                     else:
                         err_msg= 'Element state does not allow to perform the operation'
                 else:
-                    err_msg='Element not present on the page where operation is trying to be performed'
-            if ( err_msg!=None ):
+                    err_msg = 'Element not present on the page where operation is trying to be performed'
+            else:
+                err_msg = 'Unable to find the window in which the element needs to be automated'
+            if ( err_msg ):
                 log.info( err_msg )
-                log.error( err_msg )
                 logger.print_on_console( err_msg )
         except Exception as exception:
             err_msg = desktop_constants.ERROR_MSG
@@ -104,12 +106,12 @@ class Radio_Checkbox_keywords():
         return status,result,verb,err_msg
 
     def unselect_checkbox(self,element ,parent ,*args):
-        status=desktop_constants.TEST_RESULT_FAIL
-        result=desktop_constants.TEST_RESULT_FALSE
+        status = desktop_constants.TEST_RESULT_FAIL
+        result = desktop_constants.TEST_RESULT_FALSE
         verb = OUTPUT_CONSTANT
-        err_msg=None
+        err_msg = None
         try:
-            if ( desktop_launch_keywords.window_name!=None ):
+            if ( desktop_launch_keywords.window_name ):
                 log.info('Recieved element from the desktop dispatcher')
                 dektop_element = element
                 verify_obj = Text_Box()
@@ -120,7 +122,7 @@ class Radio_Checkbox_keywords():
                 log.debug(check)
                 if ( check ):
                     log.info('Parent matched')
-                    if( element.is_enabled() ):
+                    if ( element.is_enabled() ):
                         state = self.get_status(element,parent)
                         if ( state[2] == 'UnChecked' ):
                             err_msg='Check box is already unchecked'
@@ -135,10 +137,11 @@ class Radio_Checkbox_keywords():
                     else:
                         err_msg= 'Element state does not allow to perform the operation'
                 else:
-                    err_msg='Element not present on the page where operation is trying to be performed'
-            if ( err_msg!=None ):
+                    err_msg = 'Element not present on the page where operation is trying to be performed'
+            else:
+                err_msg = 'Unable to find the window in which the element needs to be automated'
+            if ( err_msg ):
                 log.info( err_msg )
-                log.error( err_msg )
                 logger.print_on_console( err_msg )
         except Exception as exception:
             err_msg = desktop_constants.ERROR_MSG
@@ -147,50 +150,55 @@ class Radio_Checkbox_keywords():
         return status,result,verb,err_msg
 
     def get_status(self,element ,parent ,*args):
-        status=desktop_constants.TEST_RESULT_FAIL
-        result=desktop_constants.TEST_RESULT_FALSE
-        flag=None
-        err_msg=None
+        status = desktop_constants.TEST_RESULT_FAIL
+        result = desktop_constants.TEST_RESULT_FALSE
+        flag = None
+        err_msg = None
         try:
-            if ( desktop_launch_keywords.window_name!=None ):
+            if ( desktop_launch_keywords.window_name ):
                 verify_obj = Text_Box()
                 check = verify_obj.verify_parent(element,parent)
                 if ( check ):
-                    tag = element.friendly_class_name()
-                    if ( element.backend.name=='uia' ):
-                        if ( tag=='RadioButton' ):
-                            status = element.is_selected()
-                        elif ( tag=='CheckBox' ):
-                            status = element.get_toggle_state()
+                    log.info('Parent matched')
+                    if ( element.is_enabled() ):
+                        tag = element.friendly_class_name()
+                        if ( element.backend.name == 'uia' ):
+                            if ( tag == 'RadioButton' ):
+                                status = element.is_selected()
+                            elif ( tag == 'CheckBox' ):
+                                status = element.get_toggle_state()
+                        else:
+                            status = element.get_check_state()
+                        if( tag == 'CheckBox' ):
+                            if( status == 1 ):
+                                flag = 'Checked'
+                                status=desktop_constants.TEST_RESULT_PASS
+                                result = desktop_constants.TEST_RESULT_TRUE
+                            else:
+                                flag = 'UnChecked'
+                                status = desktop_constants.TEST_RESULT_PASS
+                                result = desktop_constants.TEST_RESULT_TRUE
+                        if( tag == 'RadioButton' ):
+                            if ( status == 1 ):
+                                flag = 'Selected'
+                                status = desktop_constants.TEST_RESULT_PASS
+                                result = desktop_constants.TEST_RESULT_TRUE
+                            else:
+                                flag = 'UnSelected'
+                                status = desktop_constants.TEST_RESULT_PASS
+                                result = desktop_constants.TEST_RESULT_TRUE
+                        elif( tag == 'Button' ):
+                            flag = status
+                            status=desktop_constants.TEST_RESULT_PASS
+                            result = desktop_constants.TEST_RESULT_TRUE
                     else:
-                        status = element.get_check_state()
-                    if( tag == 'CheckBox' ):
-                        if( status == 1 ):
-                            flag = 'Checked'
-                            status=desktop_constants.TEST_RESULT_PASS
-                            result = desktop_constants.TEST_RESULT_TRUE
-                        else:
-                            flag = 'UnChecked'
-                            status=desktop_constants.TEST_RESULT_PASS
-                            result = desktop_constants.TEST_RESULT_TRUE
-                    if( tag == 'RadioButton' ):
-                        if( status == 1 ):
-                            flag = 'Selected'
-                            status=desktop_constants.TEST_RESULT_PASS
-                            result = desktop_constants.TEST_RESULT_TRUE
-                        else:
-                            flag = 'UnSelected'
-                            status=desktop_constants.TEST_RESULT_PASS
-                            result = desktop_constants.TEST_RESULT_TRUE
-                    elif( tag == 'Button' ):
-                        flag = status
-                        status=desktop_constants.TEST_RESULT_PASS
-                        result = desktop_constants.TEST_RESULT_TRUE
+                        err_msg= 'Element state does not allow to perform the operation'
                 else:
-                    err_msg='Element not present on the page where operation is trying to be performed'
-            if ( err_msg!=None ):
+                    err_msg = 'Element not present on the page where operation is trying to be performed'
+            else:
+                err_msg = 'Unable to find the window in which the element needs to be automated'
+            if ( err_msg ):
                 log.info( err_msg )
-                log.error( err_msg )
                 logger.print_on_console( err_msg )
         except Exception as exception:
             err_msg = desktop_constants.ERROR_MSG
