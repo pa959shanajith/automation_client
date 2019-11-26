924d215c649c957480f94b2abf4c9e4c925cf75e
ANAS AHMED <anas.ahmed@slkgroup.com>
2019-11-26 18:07:13 +0530

    #3308

--- plugins/Core/controller.py
+++ plugins/Core/controller.py
@@ -535,12 +535,11 @@ class Controller():
                         elif result:
                             logger.print_on_console('Result obtained is: ',result[1])
                     else:
-                        logger.print_on_console('Result obtained is ',",".join([str(display_keyword_response[local_cont.i])
+                        if (tsp.apptype.lower()!='desktop') : logger.print_on_console('Result obtained is ',",".join([str(display_keyword_response[local_cont.i])
                         if not isinstance(display_keyword_response[local_cont.i],str) else display_keyword_response[local_cont.i] for local_cont.i in range(len(display_keyword_response))]))
             else:
                 logger.print_on_console('Result obtained exceeds max. Limit, please use writeToFile keyword.')
-        log.info('Result obtained is: ')
-        log.info(display_keyword_response)
+        log.info('Result obtained is: '+str(display_keyword_response))
         if tsp.apptype.lower()=='desktop' or tsp.apptype.lower()=='sap' or tsp.apptype.lower()=='desktopjava' or (tsp.cord!='' and tsp.cord!=None):
             if result[2]!='9cc33d6fe25973868b30f4439f09901a' and tsp.name.lower()!='verifytextiris':
                 logger.print_on_console('Result obtained is: ',result[2])
