c9e543a454c026904487bd75f9f3b11e0202a8a5
keerthana.pai <keerthana.pai@slkgroup.com>
2020-07-28 15:00:00 +0530

    Defect #11596 fix for headless scraping issue

--- plugins/Core/core.py
+++ plugins/Core/core.py
@@ -295,6 +295,13 @@ class MainNamespace(BaseNamespace):
             args = list(args)
             d = args[0]
             action = d['action']
+            configvalues = readconfig.configvalues
+            headless_mode = str(configvalues['headless_mode'])=='Yes' 
+            if headless_mode:
+                log.info("Scraping cannot be performed in headless mode")
+                logger.print_on_console("Scraping cannot be performed in headless mode") 
+                socketIO.emit('scrape','Terminate')  
+                return None         
             data = {}
             if action == 'userobject':
                 core_utils.get_all_the_imports('WebScrape')
