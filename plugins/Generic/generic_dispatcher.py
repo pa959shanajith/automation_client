979380013dcbcd06237ac2eff29882f224f98f63
Harish G <harish.g@slksoft.com>
2020-08-13 15:13:20 +0530

    Bugs#12160 and 11701

--- plugins/Generic/generic_dispatcher.py
+++ plugins/Generic/generic_dispatcher.py
@@ -215,14 +215,14 @@ class GenericKeywordDispatcher:
                     if (str(output)==''):
                         output=output
                     #split output for static variable
-                    if (str(output[0]).startswith("|")):
+                    elif (str(output[0]).startswith("|")):
                         if ';' in output:
                             output = output.split(';')
                         else:
                             opList=[]
                             opList.append(output)
                             output = opList
-                    if str(output[0]).startswith("|") and str(output[0]).endswith("|"):
+                    elif str(output[0]).startswith("|") and str(output[0]).endswith("|"):
                         import handler
                         import controller
                         con = controller.Controller()
