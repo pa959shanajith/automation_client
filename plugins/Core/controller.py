f77b5b0efc84e8ca731cad4e21050988d10768c5
Aditya K Nayak <aditya.k@slksoft.com>
2020-12-22 11:41:53 +0530

    Fix for #17330

--- plugins/Core/controller.py
+++ plugins/Core/controller.py
@@ -571,7 +571,8 @@ class Controller():
                     else:
                         keyword_lower = tsp.name.lower()
                         #list containing keywords that should not print output on console, add keyword here to stop printing
-                        exception_list = ['getxmlblockdata','findimageinpdf','comparepdfs']
+                        #Fix for #17330 Addition of getAllValues in exception_list 
+                        exception_list = ['getxmlblockdata','findimageinpdf','comparepdfs','getallvalues']
                         if (tsp.apptype.lower()!='desktop' and keyword_lower not in exception_list) : logger.print_on_console('Result obtained is ',",".join([str(display_keyword_response[local_cont.i])
                         if not isinstance(display_keyword_response[local_cont.i],str) else display_keyword_response[local_cont.i] for local_cont.i in range(len(display_keyword_response))]))
             else:
