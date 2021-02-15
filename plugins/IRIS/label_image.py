096b19fe4032491858b387610ec098cf882efee3
ANAS AHMED <anas.ahmed@slkgroup.com>
2021-02-15 19:42:12 +0530

    #18124: Create an accurate prediction Model

--- plugins/IRIS/label_image.py
+++ plugins/IRIS/label_image.py
@@ -37,33 +37,27 @@ def load_graph(model_file):
   return graph
 
 
-def read_tensor_from_image_file(file_name,
-                                input_height=299,
-                                input_width=299,
-                                input_mean=0,
-                                input_std=255):
-  input_name = "file_reader"
-  output_name = "normalized"
-  file_reader = tf.read_file(file_name, input_name)
-  if file_name.endswith(".png"):
-    image_reader = tf.image.decode_png(
-        file_reader, channels=3, name="png_reader")
-  elif file_name.endswith(".gif"):
-    image_reader = tf.squeeze(
-        tf.image.decode_gif(file_reader, name="gif_reader"))
-  elif file_name.endswith(".bmp"):
-    image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
-  else:
-    image_reader = tf.image.decode_jpeg(
-        file_reader, channels=3, name="jpeg_reader")
-  float_caster = tf.cast(image_reader, tf.float32)
-  dims_expander = tf.expand_dims(float_caster, 0)
-  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
-  result = sess.run(normalized)
+def read_tensor_from_image_file(file_name,input_height=299,input_width=299,input_mean=0,input_std=255):
+    with tf.Graph().as_default():
+        input_name = "file_reader"
+        output_name = "normalized"
+        file_reader = tf.read_file(file_name, input_name)
+        if file_name.endswith(".png"):
+            image_reader = tf.image.decode_png(file_reader, channels=3, name="png_reader")
+        elif file_name.endswith(".gif"):
+            image_reader = tf.squeeze(tf.image.decode_gif(file_reader, name="gif_reader"))
+        elif file_name.endswith(".bmp"):
+            image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
+        else:
+            image_reader = tf.image.decode_jpeg(file_reader, channels=3, name="jpeg_reader")
+        float_caster = tf.cast(image_reader, tf.float32)
+        dims_expander = tf.expand_dims(float_caster, 0)
+        resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+        normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+        sess = tf.Session()
+        result = sess.run(normalized)
 
-  return result
+        return result
 
 
 def load_labels(label_file):
@@ -78,13 +72,15 @@ class LabelImage():
     def start(self,view):
         try:
             if SYSTEM_OS=='Darwin':
-                model_file = os.environ['AVO_ASSURE_HOME'] + '/lib/python2.7/site-packages/prediction/retrained_graph.pb'
-                label_file = os.environ['AVO_ASSURE_HOME'] + '/lib/python2.7/site-packages/prediction/retrained_labels.txt'
+                model_file = os.environ['AVO_ASSURE_HOME'] + '/lib/python3.7/site-packages/prediction/retrained_graph.pb'
+                label_file = os.environ['AVO_ASSURE_HOME'] + '/lib/python3.7/site-packages/prediction/retrained_labels.txt'
             else:
                 model_file = os.environ['AVO_ASSURE_HOME'] + '/Lib/site-packages/prediction/retrained_graph.pb'
                 label_file = os.environ['AVO_ASSURE_HOME'] + '/Lib/site-packages/prediction/retrained_labels.txt'
-            input_height = 299
-            input_width = 299
+##            input_height = 299 # for inception_v3
+##            input_width = 299 # for inception_v3
+            input_height = 224
+            input_width = 224
             input_mean = 0
             input_std = 255
             #input_layer = "input"
