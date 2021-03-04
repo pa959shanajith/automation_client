6155d01e289d7d20c66482186dfa3889b1b7b8ab
Rakshak Kamath <rakshak.kamath@slkgroup.com>
2021-03-04 19:41:22 +0530

    #19100 : Salesforce mouseclick fixed

--- plugins/Web/table_keywords.py
+++ plugins/Web/table_keywords.py
@@ -611,6 +611,7 @@ class TableOperationKeywords():
                                     methodoutput=TEST_RESULT_TRUE
                                     local_tk.log.info('Got the result : %s',str(tooltip))
                                     logger.print_on_console('Got the result : ',str(tooltip))
+                                    break
                                 else:
                                     tooltip=None
                                     err_msg = 'No cell tool tip present for the cell'
@@ -693,17 +694,18 @@ class TableOperationKeywords():
                                     expected_value=input_val[2].strip()
                                     coreutilsobj=core_utils.CoreUtils()
                                     expected_value=coreutilsobj.get_UTF_8(expected_value)
-                                if(verifytooltip == expected_value):
-                                    local_tk.log.info('Got the result : %s', str(verifytooltip))
-                                    logger.print_on_console('Got the result : ',str(verifytooltip))
-                                    status=TEST_RESULT_PASS
-                                    methodoutput=TEST_RESULT_TRUE
-                                else:
-                                    local_tk.log.info(ERROR_CODE_DICT['ERR_VALUES_DOESNOT_MATCH'])
-                                    err_msg = ERROR_CODE_DICT['ERR_VALUES_DOESNOT_MATCH']
-                                    logger.print_on_console(ERROR_CODE_DICT['ERR_VALUES_DOESNOT_MATCH'])
-                                    logger.print_on_console('Actual value is : ',str(verifytooltip))
-                                    logger.print_on_console('Expected value is : ',str(expected_value))
+                                    break
+                            if(verifytooltip == expected_value):
+                                local_tk.log.info('Got the result : %s', str(verifytooltip))
+                                logger.print_on_console('Got the result : ',str(verifytooltip))
+                                status=TEST_RESULT_PASS
+                                methodoutput=TEST_RESULT_TRUE
+                            else:
+                                local_tk.log.info(ERROR_CODE_DICT['ERR_VALUES_DOESNOT_MATCH'])
+                                err_msg = ERROR_CODE_DICT['ERR_VALUES_DOESNOT_MATCH']
+                                logger.print_on_console(ERROR_CODE_DICT['ERR_VALUES_DOESNOT_MATCH'])
+                                logger.print_on_console('Actual value is : ',str(verifytooltip))
+                                logger.print_on_console('Expected value is : ',str(expected_value))
                     elif(len(input_val) > 1):
                         row_number=int(input_val[0])
                         col_number=int(input_val[1])
