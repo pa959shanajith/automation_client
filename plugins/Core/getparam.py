f0dddc615854a5adf346bd7f85b664b4375e5f39
Aditya K Nayak <aditya.k@slksoft.com>
2021-08-04 10:55:24 +0530

    changes

--- plugins/Core/getparam.py
+++ plugins/Core/getparam.py
@@ -58,6 +58,7 @@ class GetParam():
         self.testcase_details=testcase_details
 
     def print_step(self):
+        ##        logger.print_on_console('Step: '+str(self.index)+' '+self.name+' '+str(self.inputval)+' '+self.testscript_name+' '+str(self.info_dict))
         log.info('Step: '+str(self.index)+' '+self.name+' '+str(self.inputval)+' '+self.testscript_name+' '+str(self.info_dict))
 
 
@@ -597,7 +598,7 @@ class GetParam():
                 step_description='Parameterization method executed and status is Pass'
                 fileinfo = input
                 filepath = fileinfo[0]
-                data = self.invokegetparam(input, datatables)
+                data = self.invokegetparam(input,datatables)
                 handler.local_handler.paramData=data
                 startRow = None
                 endRow =None
