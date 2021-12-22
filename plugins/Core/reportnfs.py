c5c3694dc42fd1dca99a6cee8fffa7cdc556f45c
vivek.sharma <vivek.sharma@slkgroup.com>
2021-12-22 11:26:51 +0530

    reportNFS Connection Fix

--- plugins/Core/reportnfs.py
+++ plugins/Core/reportnfs.py
@@ -2,16 +2,40 @@ from minio import Minio
 import logging
 from constants import *
 import logger
+import readconfig
+import core
+server_ip = readconfig.configvalues['server_ip']
 log = logging.getLogger('screenshot_keywords.py')
+
+import urllib3
 
 class reportNFS():
 
     def __init__(self):
+        kw = core.ConnectionThread(None).get_ice_session(no_params=True)
+        cert = kw.get('cert', [None,None])
+        ca_certs = kw.get('verify', None)
+        cert_reqs = None if ca_certs == False else 'CERT_REQUIRED'
+        http_client = urllib3.PoolManager(
+                        cert_reqs=cert_reqs,
+                        headers=kw.get('headers', {}),
+                        ca_certs=ca_certs,
+                        assert_hostname=kw.get('assert_hostname', True),
+                        key_file=cert[1],cert_file=cert[0],
+                        _proxy=kw.get('proxy', {'https':None})['https'],
+                        retries=urllib3.Retry(
+                            total=5,
+                            backoff_factor=0.2,
+                            status_forcelist=[500, 502, 503, 504]
+                        )
+                )
         self.client = Minio(
-            '127.0.0.1:9000', 
+            server_ip, 
             access_key="avoassureadmin",
             secret_key="Welcome@dmin",
-            secure=False
+            # secure=False,
+            secure=True,
+            http_client=http_client
         )
 
     def saveimage(self,*args):
