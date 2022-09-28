cf728a142bf551440c92907e41d8a9e25a22adc5
Ajeya Devadiga <ajeya.devadiga@slkgroup.com>
2022-09-28 16:11:42 +0530

    #34021: Mapping list type to custom keywords.

--- plugins/Oebs/oebs_dispatcher.py
+++ plugins/Oebs/oebs_dispatcher.py
@@ -209,13 +209,13 @@ class OebsDispatcher:
                     'mousehover': ['push button','combo box'],
                     'verifybuttonname': ['push button'],
 
-                    'getallvalues': ['combo box'],
-                    'getcount': ['combo box'],
-                    'getselected': ['combo box'],
-                    'getvaluebyindex': ['combo box'],
-                    'verifyallvalues': ['combo box'],
-                    'verifycount': ['combo box'],
-                    'verifyvaluesexists' : ['combo box']
+                    'getallvalues': ['combo box',  'list'],
+                    'getcount': ['combo box', 'list'],
+                    'getselected': ['combo box', 'list'],
+                    'getvaluebyindex': ['combo box', 'list'],
+                    'verifyallvalues': ['combo box', 'list'],
+                    'verifycount': ['combo box', 'list'],
+                    'verifyvaluesexists' : ['combo box', 'list']
                     }
 
     get_ele_type={
