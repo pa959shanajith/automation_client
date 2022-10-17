1d6441821cefb273362b87a01edea654daaf6f0a
aditya nayak <aditya.k@slkgroup.com>
2022-10-17 15:25:52 +0530

    Bug #34364: Mac: Execution Reports not showing records.

--- plugins/Web/combobox_operations.py
+++ plugins/Web/combobox_operations.py
@@ -7,7 +7,8 @@ import core_utils
 import logging
 from constants import *
 import threading
-from pyrobot import Robot
+if SYSTEM_OS=='Windows':
+    from pyrobot import Robot
 from selenium.webdriver.common.keys import Keys
 local_cbo=threading.local()
 
